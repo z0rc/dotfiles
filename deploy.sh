b903a0d7fe146900527f519aaef42544792a50ad
Igor Urazov <z0rc3r@gmail.com>
2014-07-04 15:07:34 +0300

    Added basic gitconfig

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-git config --global color.ui true
 git submodule update --init --recursive
+
 cd ..
+
+mkdir -p .config/git
+
 ln -s .dotfiles/zshrc .zshrc
 ln -s .dotfiles/vimrc .vimrc
 ln -s .dotfiles/vim .vim
 ln -s .dotfiles/tmux.conf .tmux.conf
+ln -s .dotfilse/gitconfig .config/git/config
+
