d8821a3755120236d9ceeee0048544e284e0cd16
Igor Urazov <z0rc3r@gmail.com>
2014-07-08 17:43:03 +0300

    Added custome path for $MYSQL_HISTFILE

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,10 @@
 
 git submodule update --init --recursive
 
-cd mongo-hacker
-make mongo_hacker.js
-
 cd ~
 
 mkdir -p ~/.config/git
+mkdir -p ~/.local/share
 
 ln -s .dotfiles/zsh/.zshenv ~/.zshenv
 ln -s ../../.dotfiles/gitconfig ~/.config/git/config
