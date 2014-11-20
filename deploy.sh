42dfd02506772a2a4c300ac7f31d30b825bb4fd2
Igor Urazov <z0rc3r@gmail.com>
2014-11-20 14:35:51 +0200

    Added mc config

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,12 @@ make mongo_hacker.js
 
 cd ~
 
-mkdir -p ~/.config/git
+mkdir -p ~/.config/{git,mc}
 mkdir -p ~/.cache
 mkdir -p ~/.local/share
 
 ln -s .dotfiles/zsh/.zshenv ~/.zshenv
 touch .dotfiles/zsh/.zshenv.local
 ln -s ../../.dotfiles/gitconfig ~/.config/git/config
+ln -s ../../.dotfiles/mc.ini ~/.config/mc/ini
 
