9891cfc08d41e17b8006c3aee06ca5b9210d6996
Igor Urazov <z0rc3r@gmail.com>
2014-11-26 12:00:34 +0200

    Fixed deploy without $HOME
    For example at d-i preseed run

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,22 @@
 #!/bin/zsh
 
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+cd $SCRIPT_DIR
+
 git submodule update --init --recursive
 
 cd mongo-hacker
 make mongo_hacker.js
 
-cd ~
+cd $SCRIPT_DIR/..
 
-mkdir -p ~/.config/{git,mc}
-mkdir -p ~/.cache
-mkdir -p ~/.local/share
+mkdir -p .config/{git,mc}
+mkdir -p .cache
+mkdir -p .local/share
 
-ln -sf .dotfiles/zsh/.zshenv ~/.zshenv
+ln -sf .dotfiles/zsh/.zshenv .zshenv
 touch .dotfiles/zsh/.zshenv.local
-ln -sf ../../.dotfiles/gitconfig ~/.config/git/config
-ln -sf ../../.dotfiles/mc.ini ~/.config/mc/ini
+ln -sf ../../.dotfiles/gitconfig .config/git/config
+ln -sf ../../.dotfiles/mc.ini .config/mc/ini
 
