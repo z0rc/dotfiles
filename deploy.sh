f4db913a6aed14093c418f1fc40ebd68873330ca
Igor Urazov <z0rc3r@gmail.com>
2015-02-18 00:01:22 +0200

    Install cron job to pull updates every day

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,30 @@
 #!/bin/zsh
 
+# Get out current path
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
 cd $SCRIPT_DIR
 
+# Make sure submodules are installed
 git submodule update --init --recursive
 
+# Make mongo-hacker
 cd mongo-hacker
 make mongo_hacker.js
 
+# Create required directories
 cd $SCRIPT_DIR/..
-
 mkdir -p .config/{git,mc}
 mkdir -p .cache
 mkdir -p .local/share
 
+# Link config files
 ln -sf .dotfiles/zsh/.zshenv .zshenv
 touch .dotfiles/zsh/.zshenv.local
 ln -sf ../../.dotfiles/gitconfig .config/git/config
 ln -sf ../../.dotfiles/mc.ini .config/mc/ini
+
+# Install crontab task to pull updates every midnight
+CRON_TASK="cd $SCRIPTDIR/.dotfiles && git pull"
+CRON_SCHEDULE="0 0 * * * $CRON_TASK"
+cat <(fgrep -i -v "$CRON_TASK" <(crontab -l)) <(echo "$CRON_SCHEDULE") | crontab -
 
