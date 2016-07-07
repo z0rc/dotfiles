63faba420ae130a75b3e7963d2711bd5295e60e0
Igor Urazov <z0rc3r@gmail.com>
2016-07-07 11:53:20 +0300

    Sanitize deploy script
    Don't use relative links.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 
+# Default XDG paths
+XDG_CACHE_HOME=$HOME/.cache
+XDG_CONFIG_HOME=$HOME/.config
+XDG_DATA_HOME=$HOME/.local/share
+
 # Get out current path
 SCRIPT_DIR="${0:A:h}"
 cd $SCRIPT_DIR
@@ -16,28 +21,26 @@ cd mongo-hacker
 make mongo_hacker.js
 
 # Create required directories
-cd $SCRIPT_DIR/..
-mkdir -p .config/{git,mc,htop}
-mkdir -p .cache/{vim,zsh}
-mkdir -p .local/share/{pyenv/plugins,rbenv/plugins,zsh}
+mkdir -p "$XDG_CONFIG_HOME/{git,mc,htop}"
+mkdir -p "$XDG_CACHE_HOME/{vim,zsh}"
+mkdir -p "$XDG_DATA_HOME/{pyenv/plugins,rbenv/plugins,zsh}"
 
 # Link config files
-ln -sf .dotfiles/zsh/.zshenv .zshenv
-touch .dotfiles/zsh/.zshenv.local
-ln -sf ../../.dotfiles/gitconfig .config/git/config
-ln -sf ../../.dotfiles/gitattributes .config/git/attributes
-ln -sf ../../.dotfiles/mc.ini .config/mc/ini
-ln -sf ../../.dotfiles/htoprc .config/htop/htoprc
+ln -sf "$SCRIPT_DIR/zsh/.zshenv" "$HOME/.zshenv"
+ln -sf "$SCRIPT_DIR/gitconfig" "$XDG_CONFIG_HOME/git/config"
+ln -sf "$SCRIPT_DIR/gitattributes" "$XDG_CONFIG_HOME/git/attributes"
+ln -sf "$SCRIPT_DIR/mc.ini" "$XDG_CONFIG_HOME/mc/ini"
+ln -sf "$SCRIPT_DIR/htoprc" "$XDG_CONFIG_HOME/htop/htoprc"
 
 # Link pyenv plugins to $PYENV_ROOT
-ln -snf ../../../../.dotfiles/pyenv/plugins/python-build .local/share/pyenv/plugins/python-build
-ln -snf ../../../../.dotfiles/pyenv-virtualenv .local/share/pyenv/plugins/pyenv-virtualenv
+ln -snf "$SCRIPT_DIR/pyenv/plugins/python-build" "$XDG_DATA_HOME/pyenv/plugins/python-build"
+ln -snf "$SCRIPT_DIR/pyenv-virtualenv" "$XDG_DATA_HOME/pyenv/plugins/pyenv-virtualenv"
 
 # Link rbenv plugins to $RBENV_ROOT
-ln -snf ../../../../.dotfiles/ruby-build .local/share/rbenv/plugins/ruby-build
-ln -snf ../../../../.dotfiles/rbenv-ctags .local/share/rbenv/plugins/rbenv-ctags
-ln -snf ../../../../.dotfiles/rbenv-default-gems .local/share/rbenv/plugins/rbenv-default-gems
-ln -snf ../../../.dotfiles/default-gems .local/share/rbenv/default-gems
+ln -snf "$SCRIPT_DIR/ruby-build" "$XDG_DATA_HOME/rbenv/plugins/ruby-build"
+ln -snf "$SCRIPT_DIR/rbenv-ctags" "$XDG_DATA_HOME/rbenv/plugins/rbenv-ctags"
+ln -snf "$SCRIPT_DIR/rbenv-default-gems" "$XDG_DATA_HOME/rbenv/plugins/rbenv-default-gems"
+ln -snf "$SCRIPT_DIR/default-gems" "$XDG_DATA_HOME/rbenv/default-gems"
 
 # Install crontab task to pull updates every midnight
 CRON_TASK="cd $SCRIPT_DIR && git pull >/dev/null 2>&1"
