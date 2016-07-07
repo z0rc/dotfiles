46e1d450ecaaa56a85ed4c5d5b4c6f7edf616920
Igor Urazov <z0rc3r@gmail.com>
2016-07-07 15:16:34 +0300

    Organized configs under subfolder

--- deploy.sh
+++ deploy.sh
@@ -27,10 +27,10 @@ mkdir -p $XDG_DATA_HOME/{pyenv/plugins,rbenv/plugins,zsh}
 
 # Link config files
 ln -sf "$SCRIPT_DIR/zsh/zshenv" "$HOME/.zshenv"
-ln -sf "$SCRIPT_DIR/gitconfig" "$XDG_CONFIG_HOME/git/config"
-ln -sf "$SCRIPT_DIR/gitattributes" "$XDG_CONFIG_HOME/git/attributes"
-ln -sf "$SCRIPT_DIR/mc.ini" "$XDG_CONFIG_HOME/mc/ini"
-ln -sf "$SCRIPT_DIR/htoprc" "$XDG_CONFIG_HOME/htop/htoprc"
+ln -sf "$SCRIPT_DIR/configs/gitconfig" "$XDG_CONFIG_HOME/git/config"
+ln -sf "$SCRIPT_DIR/configs/gitattributes" "$XDG_CONFIG_HOME/git/attributes"
+ln -sf "$SCRIPT_DIR/configs/mc.ini" "$XDG_CONFIG_HOME/mc/ini"
+ln -sf "$SCRIPT_DIR/configs/htoprc" "$XDG_CONFIG_HOME/htop/htoprc"
 
 # Link pyenv plugins to $PYENV_ROOT
 ln -snf "$SCRIPT_DIR/pyenv/pyenv/plugins/python-build" "$XDG_DATA_HOME/pyenv/plugins/python-build"
