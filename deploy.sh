bf08169a5cc2e2d9004ac444b968724ad6c07684
Igor Urazov <z0rc3r@gmail.com>
2016-10-13 14:46:52 +0300

    Slight better wording

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ XDG_CACHE_HOME="$HOME/.cache"
 XDG_CONFIG_HOME="$HOME/.config"
 XDG_DATA_HOME="$HOME/.local/share"
 
-# Get out current path
+# Get the current path
 SCRIPT_DIR="${0:A:h}"
 cd "$SCRIPT_DIR"
 
