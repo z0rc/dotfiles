beaff84fc4cb0c2c8ecea1dd5ff2259b63e5cbe8
Igor Urazov <z0rc3r@gmail.com>
2017-01-05 13:50:37 +0200

    Replace async init with lazy-load

--- zsh/rc.d/14_rbpynod_env.zsh
+++ zsh/rc.d/14_rbpynod_env.zsh
@@ -1,21 +1,15 @@
 # Don't indicate virtualenv in pyenv, indication is done in pure
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 
-# Init async callbacks
-env_init_callback() {
-    eval "$3" # $3 is stdout
-    typeset -U path # sanitize $PATH after init
-}
-
-async_init
-async_start_worker env_init_worker -n
-async_register_callback env_init_worker env_init_callback
-
+# Lazy init wrapper on first call
 wrappers=(pyenv rbenv nodenv)
 for wrapper in "${wrappers[@]}"; do
-    export "${wrapper:u}_ROOT"="$XDG_DATA_HOME/$wrapper"
-    PATH="$DOTFILES/$wrapper/$wrapper/bin:$PATH"
-
-    async_job env_init_worker $wrapper init -
+    eval "$wrapper() {
+        unset -f $wrapper
+        export "${wrapper:u}_ROOT"="\$XDG_DATA_HOME/$wrapper"
+        PATH="\$DOTFILES/$wrapper/$wrapper/bin:\$PATH"
+        eval \"\$(command $wrapper init -)\"
+        $wrapper \$@
+    }"
 done
 unset wrappers wrapper
