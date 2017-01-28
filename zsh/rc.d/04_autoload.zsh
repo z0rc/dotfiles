81f8afb55a0dc11169ff8ba8b59679e3b22097d0
Igor Urazov <z0rc3r@gmail.com>
2017-01-28 23:11:17 +0200

    Enable bracketed paste magic

--- zsh/rc.d/04_autoload.zsh
+++ zsh/rc.d/04_autoload.zsh
@@ -7,3 +7,7 @@ zle -N self-insert url-quote-magic
 
 # MIME aliases
 autoload -U zsh-mime-setup && zsh-mime-setup
+
+# Bracketed paste magic
+autoload -Uz bracketed-paste-magic
+zle -N bracketed-paste bracketed-paste-magic
