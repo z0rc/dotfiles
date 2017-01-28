8a3d3df541847cc97f491f831aba980246842f74
Igor Urazov <z0rc3r@gmail.com>
2017-01-28 23:03:48 +0200

    Switched to official async z-asug

--- zsh/rc.d/57_autosuggestions.zsh
+++ zsh/rc.d/57_autosuggestions.zsh
@@ -1,9 +1,2 @@
 # Autosuggestions plugin
 source "$ZSHDIR/plugins/autosuggestions/zsh-autosuggestions.zsh"
-# Hack to init zsh-asug only once
-# https://github.com/zsh-users/zsh-autosuggestions/issues/136#issuecomment-196640897
-_zsh_autosuggest_start () {
-    add-zsh-hook -d precmd _zsh_autosuggest_start
-    _zsh_autosuggest_check_deprecated_config
-    _zsh_autosuggest_bind_widgets
-}
