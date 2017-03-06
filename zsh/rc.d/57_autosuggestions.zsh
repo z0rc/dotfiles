d93079e9e17811275a945d056be19bb40b4af312
Igor Urazov <z0rc3r@gmail.com>
2017-03-06 10:48:47 +0700

    Enabled z-asug compatibility with bpm

--- zsh/rc.d/57_autosuggestions.zsh
+++ zsh/rc.d/57_autosuggestions.zsh
@@ -1,2 +1,7 @@
 # Autosuggestions plugin
 source "$ZSHDIR/plugins/autosuggestions/zsh-autosuggestions.zsh"
+# Compatability with bracketed-paste-magic
+function z-asug-d { zle autosuggest-disable; }
+function z-asug-e { zle autosuggest-enable; }
+zstyle :bracketed-paste-magic paste-init z-asug-d
+zstyle :bracketed-paste-magic paste-finish z-asug-e
