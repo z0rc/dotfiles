db30912e0f3af7caa2e98bf633fd2442fa9f95ae
Igor Urazov <z0rc3r@gmail.com>
2017-04-26 21:20:59 -0500

    Added pure support of rbenv indication

--- zsh/rc.d/08_pure.zsh
+++ zsh/rc.d/08_pure.zsh
@@ -7,20 +7,22 @@ source "$ZSHDIR/plugins/pure/pure.zsh"
 # Indicate that shell is running under Midnight Commander or ranger
 if [[ -n "$MC_SID" ]]; then
     _pure_indicate_filemanager() {
-        preprompt+=("[mc]")
+        preprompt+=("%F{white}[mc]%f")
     }
 elif [[ -n "$RANGER_LEVEL" ]]; then
     _pure_indicate_filemanager() {
-        preprompt+=("[ranger]")
+        preprompt+=("%F{white}[ranger]%f")
     }
 else
     _pure_indicate_filemanager() {}
 fi
 
 # Indicate virtualenv environment
-_pure_indicate_virtualenv() {
-    if [[ -n "$VIRTUAL_ENV" ]]; then
+_pure_indicate_env() {
+    if [[ -n "${VIRTUAL_ENV}" ]]; then
         preprompt+=("%F{green}venv:${VIRTUAL_ENV:t}%f")
+    elif [[ -n "$RBENV_VERSION" ]]; then
+        preprompt+=("%F{green}rbenv:${RBENV_VERSION}%f")
     fi
 }
 
@@ -28,6 +30,6 @@ _pure_indicate_virtualenv() {
 prompt_pure_pieces=(
     _pure_indicate_filemanager
     ${prompt_pure_pieces:0:1}
-    _pure_indicate_virtualenv
+    _pure_indicate_env
     ${prompt_pure_pieces:1}
 )
