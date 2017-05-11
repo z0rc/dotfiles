1fcefefea2575027becfda443adfff64e3d93172
Igor Urazov <z0rc3r@gmail.com>
2017-05-11 12:17:50 +0300

    Updated submodules
    Enabled async autosuggestions too.

--- zsh/rc.d/57_autosuggestions.zsh
+++ zsh/rc.d/57_autosuggestions.zsh
@@ -5,3 +5,5 @@ function z-asug-d { zle autosuggest-disable; }
 function z-asug-e { zle autosuggest-enable; }
 zstyle :bracketed-paste-magic paste-init z-asug-d
 zstyle :bracketed-paste-magic paste-finish z-asug-e
+# Enable experimental async autosuggestions
+ZSH_AUTOSUGGEST_USE_ASYNC=1
