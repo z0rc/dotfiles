72e28c31c1f34873cbc945e1f452176b1e04f4d0
Igor Urazov <z0rc3r@gmail.com>
2017-05-17 15:07:49 +0300

    Enabled history substring fuzzy search

--- zsh/rc.d/58_history_substring.zsh
+++ zsh/rc.d/58_history_substring.zsh
@@ -1,5 +1,6 @@
 # History substring search plugin
 source "$ZSHDIR/plugins/history-substring-search/zsh-history-substring-search.zsh"
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=green,fg=white'
+HISTORY_SUBSTRING_SEARCH_FUZZY=1
 bindkey "${key[Up]}"   history-substring-search-up
 bindkey "${key[Down]}" history-substring-search-down
