e71c833c69693067b0b038cce5ed30ac6fb16fad
Igor Urazov <z0rc3r@gmail.com>
2017-05-19 12:08:08 +0300

    More grc aliases fine tuning

--- zsh/rc.d/01_setopt.zsh
+++ zsh/rc.d/01_setopt.zsh
@@ -1,7 +1,7 @@
 setopt HIST_IGNORE_ALL_DUPS # remove all earlier duplicate lines
 setopt APPEND_HISTORY # history appends to existing file
 setopt SHARE_HISTORY # import new commands from the history file also in other zsh-session
-setopt EXTENDED_HISTORY # save each command's beginning timestamp and the duration to the history file
+setopt EXTENDED_HISTORY # save each commands beginning timestamp and the duration to the history file
 setopt HIST_REDUCE_BLANKS # trim multiple insgnificant blanks in history
 setopt HIST_IGNORE_SPACE # don’t store lines starting with space
 setopt EXTENDED_GLOB
@@ -18,3 +18,4 @@ setopt AUTO_PARAM_SLASH
 setopt LIST_TYPES
 setopt HASH_LIST_ALL # whenever a command completion is attempted, make sure the entire command path is hashed first
 setopt COMPLETE_IN_WORD # allow completion from within a word/phrase
+setopt COMPLETE_ALIASES # keep completion for aliased commands
--- zsh/rc.d/17_grc.zsh
+++ zsh/rc.d/17_grc.zsh
@@ -1,7 +1,5 @@
 # Alias commands supported by grc
-if (( $+commands[grc] )); then
-    alias colorify="grc -es --colour=auto"
-
+if (( ${+commands[grc]} )); then
     grc_commands=(blkid df dig dnf du env free gcc getfacl getsebool
                   ifconfig ip iptables last lsattr lsblk lsmod lspci
                   mount mtr netstat nmap ping ps pv semanage ss stat
@@ -9,13 +7,13 @@ if (( $+commands[grc] )); then
                   uptime vmstat wdiff)
 
     for grc_command in ${grc_commands[@]}; do
-        if [[ -f "/usr/local/share/grc/conf.${grc_command}" ]] || [[ -f "/usr/share/grc/conf.${grc_command}" ]]; then
-            alias ${grc_command}="colorify ${grc_command}"
+        if (( ${+commands[$grc_command]} )) && [[ -f "/usr/local/share/grc/conf.${grc_command}" ]] || [[ -f "/usr/share/grc/conf.${grc_command}" ]]; then
+            alias ${grc_command}="grc --color=auto ${grc_command}"
         fi
     done
 
-    for grc_command in make ld w who; do
-        alias ${grc_command}="colorify ${grc_command}"
+    for grc_command in make w who; do
+        alias ${grc_command}="grc --color=auto ${grc_command}"
     done
 
     unset grc_commands grc_command
