84277594e94161af625a7979f1cc280f48659e99
Igor Urazov <z0rc3r@gmail.com>
2017-07-18 15:04:19 +0300

    Linted ssh auth socket symlinking

--- zsh/env.d/02_ssh_auth_sock.zsh
+++ zsh/env.d/02_ssh_auth_sock.zsh
@@ -1,9 +1,9 @@
 # Keep SSH_AUTH_SOCK valid across several attachments to the remote tmux session
-if [[ `whoami` != root ]]; then
-    if [[ -S "$XDG_RUNTIME_DIR/ssh-agent.socket" ]]; then
-        ln -sf "$XDG_RUNTIME_DIR/ssh-agent.socket" "$HOME/.ssh/ssh_auth_sock"
-    elif [[ -S "$SSH_AUTH_SOCK" ]] && [[ ! -h "$SSH_AUTH_SOCK" ]] && [[ "$SSH_AUTH_SOCK" != "$HOME/.ssh/ssh_auth_sock" ]]; then
-        ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/ssh_auth_sock"
+if (( EUID != 0 )); then
+    if [[ -S "${XDG_RUNTIME_DIR}/ssh-agent.socket" ]]; then
+        ln -sf "${XDG_RUNTIME_DIR}/ssh-agent.socket" "${HOME}/.ssh/ssh_auth_sock"
+    elif [[ -S "${SSH_AUTH_SOCK}" ]] && [[ ! -h "${SSH_AUTH_SOCK}" ]] && [[ "${SSH_AUTH_SOCK}" != "${HOME}/.ssh/ssh_auth_sock" ]]; then
+        ln -sf "${SSH_AUTH_SOCK}" "${HOME}/.ssh/ssh_auth_sock"
     fi
-    export SSH_AUTH_SOCK="$HOME/.ssh/ssh_auth_sock"
+    export SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock"
 fi
