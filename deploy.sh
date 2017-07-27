7051049ee273bda21ce7094fe7f1683a857ddc23
Igor Urazov <z0rc3r@gmail.com>
2017-07-27 15:06:48 +0300

    Linted deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,39 +3,39 @@
 set -e
 
 # Default XDG paths
-XDG_CACHE_HOME="$HOME/.cache"
-XDG_CONFIG_HOME="$HOME/.config"
-XDG_DATA_HOME="$HOME/.local/share"
+XDG_CACHE_HOME="${HOME}/.cache"
+XDG_CONFIG_HOME="${HOME}/.config"
+XDG_DATA_HOME="${HOME}/.local/share"
 
 # Get the current path
 SCRIPT_DIR="${0:A:h}"
-cd "$SCRIPT_DIR"
+cd "${SCRIPT_DIR}"
 
 # Create required directories
 print "Creating required directory tree..."
-mkdir -p "$XDG_CONFIG_HOME"/{git/local,mc,htop,ranger}
-mkdir -p "$XDG_CACHE_HOME"/{vim/{backup,swap,undo},zsh}
-mkdir -p "$XDG_DATA_HOME"/{pyenv/plugins,rbenv/plugins,nodenv/plugins,luaenv/plugins,zsh,man}
-mkdir -p "$HOME"/.local/{bin,etc}
+mkdir -p "${XDG_CONFIG_HOME}"/{git/local,mc,htop,ranger}
+mkdir -p "${XDG_CACHE_HOME}"/{vim/{backup,swap,undo},zsh}
+mkdir -p "${XDG_DATA_HOME}"/{pyenv/plugins,rbenv/plugins,nodenv/plugins,luaenv/plugins,zsh,man}
+mkdir -p "${HOME}"/.local/{bin,etc}
 print "  ...done"
 
 # Link zshenv if needed
-print "Checking for custom ZDOTDIR env variable..."
+print "Checking for ZDOTDIR env variable..."
 if [[ "${ZDOTDIR}" = "${SCRIPT_DIR}/zsh" ]]; then
     print "  ...present and valid, skipping .zshenv symlink"
 else
-    ln -sf "$SCRIPT_DIR/zsh/.zshenv" "${ZDOTDIR:-$HOME}/.zshenv"
+    ln -sf "${SCRIPT_DIR}/zsh/.zshenv" "${ZDOTDIR:-${HOME}}/.zshenv"
     print "  ...failed to match this script dir, symlinking .zshenv"
 fi
 
 # Link config files
 print "Linking config files..."
-ln -sf "$SCRIPT_DIR/configs/gitconfig" "$XDG_CONFIG_HOME/git/config"
-ln -sf "$SCRIPT_DIR/configs/gitattributes" "$XDG_CONFIG_HOME/git/attributes"
-ln -sf "$SCRIPT_DIR/configs/gitignore" "$XDG_CONFIG_HOME/git/ignore"
-ln -sf "$SCRIPT_DIR/configs/mc.ini" "$XDG_CONFIG_HOME/mc/ini"
-ln -sf "$SCRIPT_DIR/configs/htoprc" "$XDG_CONFIG_HOME/htop/htoprc"
-ln -sf "$SCRIPT_DIR/configs/ranger" "$XDG_CONFIG_HOME/ranger/rc.conf"
+ln -sf "${SCRIPT_DIR}/configs/gitconfig" "${XDG_CONFIG_HOME}/git/config"
+ln -sf "${SCRIPT_DIR}/configs/gitattributes" "${XDG_CONFIG_HOME}/git/attributes"
+ln -sf "${SCRIPT_DIR}/configs/gitignore" "${XDG_CONFIG_HOME}/git/ignore"
+ln -sf "${SCRIPT_DIR}/configs/mc.ini" "${XDG_CONFIG_HOME}/mc/ini"
+ln -sf "${SCRIPT_DIR}/configs/htoprc" "${XDG_CONFIG_HOME}/htop/htoprc"
+ln -sf "${SCRIPT_DIR}/configs/ranger" "${XDG_CONFIG_HOME}/ranger/rc.conf"
 print "  ...done"
 
 # Make sure submodules are installed
@@ -50,7 +50,7 @@ mkdir -p .git/hooks
 ln -sf ../../deploy.sh .git/hooks/post-merge
 print "  ...done"
 
-if (( $+commands[make] )); then
+if (( ${+commands[make]} )); then
     # Make mongo-hacker
     print "Making mongo-hacker config..."
     pushd tools/mongo-hacker
@@ -61,56 +61,57 @@ if (( $+commands[make] )); then
     # Make install git-extras
     print "Installing git-extras..."
     pushd tools/git-extras
-    PREFIX="$HOME"/.local make install > /dev/null
+    PREFIX="${HOME}"/.local make install > /dev/null
     popd
     print "  ...done"
 fi
 
-if (( $+commands[perl] )); then
+if (( ${+commands[perl]} )); then
     # Install diff-so-fancy
     print "Installing diff-so-fancy..."
-    ln -sf "$SCRIPT_DIR/tools/diff-so-fancy/diff-so-fancy" "$HOME/.local/bin/diff-so-fancy"
-    ln -sf "$SCRIPT_DIR/tools/diff-so-fancy/third_party/diff-highlight/diff-highlight" "$HOME/.local/bin/diff-highlight"
+    ln -sf "${SCRIPT_DIR}/tools/diff-so-fancy/diff-so-fancy" "${HOME}/.local/bin/diff-so-fancy"
+    ln -sf "${SCRIPT_DIR}/tools/diff-so-fancy/third_party/diff-highlight/diff-highlight" "${HOME}/.local/bin/diff-highlight"
     print "  ...done"
 fi
 
 # Link pyenv plugins to $PYENV_ROOT
 print "Linking pyenv plugins..."
-ln -snf "$SCRIPT_DIR/pyenv/pyenv/plugins/python-build" "$XDG_DATA_HOME/pyenv/plugins/python-build"
-ln -snf "$SCRIPT_DIR/pyenv/pyenv-virtualenv" "$XDG_DATA_HOME/pyenv/plugins/pyenv-virtualenv"
-ln -snf "$SCRIPT_DIR/pyenv/pyenv-default-packages" "$XDG_DATA_HOME/pyenv/plugins/pyenv-default-packages"
-ln -snf "$SCRIPT_DIR/pyenv/default-packages" "$XDG_DATA_HOME/pyenv/default-packages"
+ln -snf "${SCRIPT_DIR}/pyenv/pyenv/plugins/python-build" "${XDG_DATA_HOME}/pyenv/plugins/python-build"
+ln -snf "${SCRIPT_DIR}/pyenv/pyenv-virtualenv" "${XDG_DATA_HOME}/pyenv/plugins/pyenv-virtualenv"
+ln -snf "${SCRIPT_DIR}/pyenv/pyenv-default-packages" "${XDG_DATA_HOME}/pyenv/plugins/pyenv-default-packages"
+ln -sf "${SCRIPT_DIR}/pyenv/default-packages" "${XDG_DATA_HOME}/pyenv/default-packages"
 print "  ...done"
 
 # Link rbenv plugins to $RBENV_ROOT
 print "Linking rbenv plugins..."
 local -a rbenv_plugins
 rbenv_plugins=("ruby-build" "rbenv-binstubs" "rbenv-chefdk" "rbenv-ctags" "rbenv-default-gems" "rbenv-env" "rbenv-man")
+local plugin
 for plugin in "${rbenv_plugins[@]}"; do
-    ln -snf "$SCRIPT_DIR/rbenv/$plugin" "$XDG_DATA_HOME/rbenv/plugins/$plugin"
+    ln -snf "${SCRIPT_DIR}/rbenv/${plugin}" "${XDG_DATA_HOME}/rbenv/plugins/${plugin}"
 done
-ln -snf "$SCRIPT_DIR/rbenv/default-gems" "$XDG_DATA_HOME/rbenv/default-gems"
+ln -sf "${SCRIPT_DIR}/rbenv/default-gems" "${XDG_DATA_HOME}/rbenv/default-gems"
 print "  ...done"
 
 # Link nodenv plugins to $NODENV_ROOT
 print "Linking nodenv plugins..."
-ln -snf "$SCRIPT_DIR/nodenv/node-build" "$XDG_DATA_HOME/nodenv/plugins/node-build"
-ln -snf "$SCRIPT_DIR/nodenv/nodenv-aliases" "$XDG_DATA_HOME/nodenv/plugins/nodenv-aliases"
-ln -snf "$SCRIPT_DIR/nodenv/nodenv-package-rehash" "$XDG_DATA_HOME/nodenv/plugins/nodenv-package-rehash"
+ln -snf "${SCRIPT_DIR}/nodenv/node-build" "${XDG_DATA_HOME}/nodenv/plugins/node-build"
+ln -snf "${SCRIPT_DIR}/nodenv/nodenv-aliases" "${XDG_DATA_HOME}/nodenv/plugins/nodenv-aliases"
+ln -snf "${SCRIPT_DIR}/nodenv/nodenv-package-rehash" "${XDG_DATA_HOME}/nodenv/plugins/nodenv-package-rehash"
 print "  ...done"
 
 # Link luaenv plugins to $LUAENV_ROOT
 print "Linking luaenv plugins..."
-ln -snf "$SCRIPT_DIR/luaenv/lua-build" "$XDG_DATA_HOME/luaenv/plugins/lua-build"
-ln -snf "$SCRIPT_DIR/luaenv/luaenv-luarocks" "$XDG_DATA_HOME/luaenv/plugins/luaenv-luarocks"
+ln -snf "${SCRIPT_DIR}/luaenv/lua-build" "${XDG_DATA_HOME}/luaenv/plugins/lua-build"
+ln -snf "${SCRIPT_DIR}/luaenv/luaenv-luarocks" "${XDG_DATA_HOME}/luaenv/plugins/luaenv-luarocks"
 print "  ...done"
 
 # Install crontab task to pull updates every midnight
 print "Installing cron job for periodic updates..."
-CRON_TASK="cd $SCRIPT_DIR && git stash >/dev/null 2>&1 && git pull >/dev/null 2>&1 && git stash pop >/dev/null 2>&1"
-CRON_SCHEDULE="0 0 * * * $CRON_TASK"
-if cat <(fgrep -i -v "$CRON_TASK" <(crontab -l)) <(echo "$CRON_SCHEDULE") | crontab -; then
+local cron_task="cd ${SCRIPT_DIR} && git stash >/dev/null 2>&1 && git pull >/dev/null 2>&1 && git stash pop >/dev/null 2>&1"
+local cron_schedule="0 0 * * * ${cron_task}"
+if cat <(fgrep -i -v "${cron_task}" <(crontab -l)) <(echo "${cron_schedule}") | crontab -; then
     print "  ...done"
 else
-    print "Please add \`cd $SCRIPT_DIR && git pull\` to your crontab or just ignore this, you can always update dotfiles manually"
+    print "Please add \`cd ${SCRIPT_DIR} && git pull\` to your crontab or just ignore this, you can always update dotfiles manually"
 fi
