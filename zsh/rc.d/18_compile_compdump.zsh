04cd77e9e73c842277d454cdfdc2a04947a205ed
Igor Urazov <z0rc3r@gmail.com>
2017-08-04 17:11:18 +0300

    Use background job for compdump compile
    There is strange issue with async runner taking too much CPU time.

--- zsh/rc.d/18_compile_compdump.zsh
+++ zsh/rc.d/18_compile_compdump.zsh
@@ -1,11 +1,7 @@
-# Asynchonously compile compdump
-_compile_compdump () {
+{
+    # Compile the completion dump to increase startup speed
     zcompdump="${XDG_CACHE_HOME}/zsh/compdump"
     if [[ -s "${zcompdump}" && (! -s "${zcompdump}.zwc" || "${zcompdump}" -nt "${zcompdump}.zwc") ]]; then
         zcompile "${zcompdump}"
     fi
-}
-
-async_start_worker compiler -u -n
-async_job compiler _compile_compdump
-unfunction _compile_compdump
+} &!
