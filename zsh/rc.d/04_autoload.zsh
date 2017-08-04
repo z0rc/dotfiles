76844d6a2b19194d0b0ab95715b3844663edb8be
Igor Urazov <z0rc3r@gmail.com>
2017-08-04 16:38:28 +0300

    Disable zsh-mime-setup
    It's heaviest startup time consumer and isn't that useful.

--- zsh/rc.d/04_autoload.zsh
+++ zsh/rc.d/04_autoload.zsh
@@ -5,9 +5,6 @@ autoload -U colors && colors
 autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
 
-# MIME aliases
-autoload -U zsh-mime-setup && zsh-mime-setup
-
 # Bracketed paste magic
 autoload -Uz bracketed-paste-magic
 zle -N bracketed-paste bracketed-paste-magic
