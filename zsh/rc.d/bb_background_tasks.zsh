472654dc4d70dff1619e8c2e3e33f24c92c71a3e
Igor Urazov <z0rc3r@gmail.com>
2017-09-12 16:14:22 +0300

    Background job to generate vim's helptags

--- zsh/rc.d/bb_background_tasks.zsh
+++ zsh/rc.d/bb_background_tasks.zsh
@@ -1,8 +1,14 @@
-# Taken from prezto, thanks for this
+# Compile the completion dump to increase startup speed
 {
-    # Compile the completion dump to increase startup speed
     zcompdump="${XDG_CACHE_HOME}/zsh/compdump"
     if [[ -s "${zcompdump}" && (! -s "${zcompdump}.zwc" || "${zcompdump}" -nt "${zcompdump}.zwc") ]]; then
         zcompile "${zcompdump}"
+    fi
+} &!
+
+# Generate vim tags
+{
+    if (( ${+commands[vim]} )); then
+        vim -c 'silent! helptags ALL | q'
     fi
 } &!
