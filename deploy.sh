3d951858134df211979b0e1ee43a518b27f386c2
Igor Urazov <z0rc3r@gmail.com>
2017-10-23 12:07:24 +0300

    Actually deploy fzf

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ cd "${SCRIPT_DIR}"
 print "Creating required directory tree..."
 mkdir -p "${XDG_CONFIG_HOME}"/{git/local,mc,htop,ranger}
 mkdir -p "${XDG_CACHE_HOME}"/{vim/{backup,swap,undo},zsh}
-mkdir -p "${XDG_DATA_HOME}"/{pyenv/plugins,rbenv/plugins,nodenv/plugins,luaenv/plugins,goenv/plugins,zsh,man}
+mkdir -p "${XDG_DATA_HOME}"/{pyenv/plugins,rbenv/plugins,nodenv/plugins,luaenv/plugins,goenv/plugins,zsh,man/man1}
 mkdir -p "${HOME}"/.local/{bin,etc}
 print "  ...done"
 
@@ -76,6 +76,10 @@ fi
 print "Installing fzf..."
 pushd tools/fzf
 ./install --bin &> /dev/null
+ln -sf "${SCRIPT_DIR}/tools/fzf/bin/fzf" "${HOME}/.local/bin/fzf"
+ln -sf "${SCRIPT_DIR}/tools/fzf/bin/fzf-tmux" "${HOME}/.local/bin/fzf-tmux"
+ln -sf "${SCRIPT_DIR}/tools/fzf/man/man1/fzf.1" "${XDG_DATA_HOME}/man/man1/fzf.1"
+ln -sf "${SCRIPT_DIR}/tools/fzf/man/man1/fzf-tmux.1" "${XDG_DATA_HOME}/man/man1/fzf-tmux.1"
 popd
 print "  ...done"
 
