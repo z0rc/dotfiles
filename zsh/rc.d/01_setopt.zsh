d3001c7d035f1fc8f9c60b110db6f8f2bcf94dd1
Igor Urazov <z0rc3r@gmail.com>
2017-11-28 12:54:41 +0200

    Expalin setopt's

--- zsh/rc.d/01_setopt.zsh
+++ zsh/rc.d/01_setopt.zsh
@@ -4,18 +4,18 @@ setopt SHARE_HISTORY # import new commands from the history file also in other z
 setopt EXTENDED_HISTORY # save each commands beginning timestamp and the duration to the history file
 setopt HIST_REDUCE_BLANKS # trim multiple insgnificant blanks in history
 setopt HIST_IGNORE_SPACE # don’t store lines starting with space
-setopt EXTENDED_GLOB
-setopt CORRECT_ALL
+setopt EXTENDED_GLOB # treat special characters as part of patterns
+setopt CORRECT_ALL # try to correct the spelling of all arguments in a line
 setopt NO_FLOW_CONTROL # disable stupid annoying keys
 setopt MULTIOS # allows multiple input and output redirections
 setopt AUTO_CD # if the command is directory and cannot be executed, perfort cd to this directory
-setopt CLOBBER
+setopt CLOBBER # allow > redirection to truncate existing files
 setopt BRACE_CCL # allow brace character class list expansion
 setopt NO_BEEP # do not beep on errors
 setopt NO_NOMATCH # try to avoid the 'zsh: no matches found...'
 setopt INTERACTIVE_COMMENTS # allow use of comments in interactive code
-setopt AUTO_PARAM_SLASH
-setopt LIST_TYPES
+setopt AUTO_PARAM_SLASH # complete folders with / at end
+setopt LIST_TYPES # mark type of completion suggestions
 setopt HASH_LIST_ALL # whenever a command completion is attempted, make sure the entire command path is hashed first
 setopt COMPLETE_IN_WORD # allow completion from within a word/phrase
 setopt COMPLETE_ALIASES # keep completion for aliased commands
