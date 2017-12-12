3c49448d4e94ce0908cf16abdb29fa4490f1ca44
Igor Urazov <z0rc3r@gmail.com>
2017-12-12 13:03:02 +0200

    More cleanup for fgl and fgb

--- zsh/rc.d/11_functions.zsh
+++ zsh/rc.d/11_functions.zsh
@@ -99,11 +99,11 @@ fcd () {
     cd "${selection/#[[:digit:]]##[[:blank:]]##~/$HOME}"
 }
 
-# git history browser with fzf
+# git log browser with fzf
 fgl () {
     git rev-parse --is-inside-work-tree &> /dev/null || return
     git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-    fzf --ansi --height 50% --no-sort --reverse --multi --bind='ctrl-s:toggle-sort' \
+    fzf --ansi --height=50% --no-sort --reverse --multi --bind='ctrl-s:toggle-sort' \
         --header='Press CTRL-S to toggle sort' \
         --preview='grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -200' |
     grep -o "[a-f0-9]\{7,\}"
@@ -112,9 +112,9 @@ fgl () {
 # git branch selector with fzf
 fgb () {
     git rev-parse --is-inside-work-tree &> /dev/null || return
-    git checkout $(git branch --color=always | grep -v '/HEAD\s' | sort |
+    git checkout $(git branch --color=always | grep -v '/HEAD\s' | sort --ignore-case |
                    fzf --ansi --height=50% --tac --preview-window=right:70% --query="${@}" \
-                       --preview='git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -200' |
+                       --preview='git log --color=always --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -200' |
                    sed 's/^..//' | cut -d' ' -f1 | sed 's#^remotes/##')
 }
 
