1d3f0ee3f5d28bae4e2148d7f670468c3eff6544
Igor Urazov <z0rc3r@gmail.com>
2018-02-27 12:34:43 +0200

    Remove sudo function that was specific for mc
    Instead we have to use sudoers env_keep

--- zsh/rc.d/13_mc_term.zsh
+++ zsh/rc.d/13_mc_term.zsh
@@ -2,12 +2,10 @@
 # Also set skin depending under which terminal we're running
 if [[ "${TERM}" = "linux" && "${EUID}" -ne 0 ]]; then
     export MC_SKIN=modarcon16-defbg
-    sudo() { if [[ "${1}" = "mc" ]]; then command sudo MC_SKIN=modarcon16root-defbg "${@}"; else command sudo "${@}"; fi; }
 elif [[ "${TERM}" = "linux" && "${EUID}" -eq 0 ]]; then
     export MC_SKIN=modarcon16root-defbg
 elif [[ "${TERM}" != "linux" && "${EUID}" -ne 0 ]]; then
     export MC_SKIN=modarin256-defbg
-    sudo() { if [[ "${1}" = "mc" ]]; then command sudo MC_SKIN=modarin256root-defbg "${@}"; else command sudo "${@}"; fi; }
 elif [[ "${TERM}" != "linux" && "${EUID}" -eq 0 ]]; then
     export MC_SKIN=modarin256root-defbg
 fi
