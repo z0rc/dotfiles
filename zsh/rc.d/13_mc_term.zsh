bcfe8186485bdbbcce02047948792ff71326c340
Igor Urazov <z0rc3r@gmail.com>
2018-03-04 19:35:45 +0200

    Switch to wrapper for mc skin
    This should help with `sudo mc` setting wrong skin.

--- zsh/rc.d/13_mc_term.zsh
+++ zsh/rc.d/13_mc_term.zsh
@@ -1,11 +1,13 @@
-# Set MC skin whether we're running under root or not
-# Also set skin depending under which terminal we're running
-if [[ "${TERM}" = "linux" && "${EUID}" -ne 0 ]]; then
-    export MC_SKIN=modarcon16-defbg
-elif [[ "${TERM}" = "linux" && "${EUID}" -eq 0 ]]; then
-    export MC_SKIN=modarcon16root-defbg
-elif [[ "${TERM}" != "linux" && "${EUID}" -ne 0 ]]; then
-    export MC_SKIN=modarin256-defbg
-elif [[ "${TERM}" != "linux" && "${EUID}" -eq 0 ]]; then
-    export MC_SKIN=modarin256root-defbg
-fi
+# Wrapper to set skip depending on terminal and effective permissions
+mc () {
+    if [[ "${TERM}" = "linux" && "${EUID}" -ne 0 ]]; then
+        export MC_SKIN=modarcon16-defbg
+    elif [[ "${TERM}" = "linux" && "${EUID}" -eq 0 ]]; then
+        export MC_SKIN=modarcon16root-defbg
+    elif [[ "${TERM}" != "linux" && "${EUID}" -ne 0 ]]; then
+        export MC_SKIN=modarin256-defbg
+    elif [[ "${TERM}" != "linux" && "${EUID}" -eq 0 ]]; then
+        export MC_SKIN=modarin256root-defbg
+    fi
+    command mc "${@}"
+}
