80de0bdea33c7ac47297b7cf6d308becf416d802
Igor Urazov <z0rc3r@gmail.com>
2018-03-12 10:55:09 +0200

    Don't create mc wrapper if mc is unavailable

--- zsh/rc.d/13_mc_term.zsh
+++ zsh/rc.d/13_mc_term.zsh
@@ -1,13 +1,15 @@
 # Wrapper to set skin depending on terminal and effective permissions
-mc () {
-    if [[ "${TERM}" = "linux" && "${EUID}" -ne 0 ]]; then
-        export MC_SKIN=modarcon16-defbg
-    elif [[ "${TERM}" = "linux" && "${EUID}" -eq 0 ]]; then
-        export MC_SKIN=modarcon16root-defbg
-    elif [[ "${TERM}" != "linux" && "${EUID}" -ne 0 ]]; then
-        export MC_SKIN=modarin256-defbg
-    elif [[ "${TERM}" != "linux" && "${EUID}" -eq 0 ]]; then
-        export MC_SKIN=modarin256root-defbg
-    fi
-    command mc "${@}"
-}
+if (( ${+commands[mc]} )); then
+    mc () {
+        if [[ "${TERM}" = "linux" && "${EUID}" -ne 0 ]]; then
+            export MC_SKIN=modarcon16-defbg
+        elif [[ "${TERM}" = "linux" && "${EUID}" -eq 0 ]]; then
+            export MC_SKIN=modarcon16root-defbg
+        elif [[ "${TERM}" != "linux" && "${EUID}" -ne 0 ]]; then
+            export MC_SKIN=modarin256-defbg
+        elif [[ "${TERM}" != "linux" && "${EUID}" -eq 0 ]]; then
+            export MC_SKIN=modarin256root-defbg
+        fi
+        command mc "${@}"
+    }
+fi
