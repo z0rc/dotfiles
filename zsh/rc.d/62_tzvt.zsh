b93c1ece3b3b3cf511010fb6338c850132ec9cdc
Igor Urazov <z0rc3r@gmail.com>
2018-07-20 13:11:11 +0300

    Add tmux-zsh-vim-title plugin

---
+++ zsh/rc.d/62_tzvt.zsh
@@ -0,0 +1,4 @@
+source "${ZDOTDIR}/plugins/tmux-zsh-vim-titles/unified-titles.plugin.zsh"
+export tzvt_zsh_update_tmux=true
+export tzvt_tmux_plugin_installed=true
+export tzvt_set_tmux_window_status=true
