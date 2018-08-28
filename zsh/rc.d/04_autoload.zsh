a157729dd8a38a1876f8f5e5242beaa308325c32
Igor Urazov <z0rc3r@gmail.com>
2018-08-28 11:41:29 +0300

    Enable zsh edit-command-line via Ctrl-x+Ctrl-e

--- zsh/rc.d/04_autoload.zsh
+++ zsh/rc.d/04_autoload.zsh
@@ -8,3 +8,8 @@ zle -N self-insert url-quote-magic
 # Bracketed paste magic
 autoload -Uz bracketed-paste-magic
 zle -N bracketed-paste bracketed-paste-magic
+
+# Fullscreen command line edit
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
