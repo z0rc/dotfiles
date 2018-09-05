151d451412fc891620df4d6cc0072af075abc450
Igor Urazov <z0rc3r@gmail.com>
2018-09-05 15:42:22 +0300

    Remove fzf-cd-widget rebind
    It's available again at default Ctrl+T key combo.

--- zsh/rc.d/59_fzf.zsh
+++ zsh/rc.d/59_fzf.zsh
@@ -5,7 +5,3 @@ if (( ${+commands[ag]} )); then
 fi
 
 source "${DOTFILES}/tools/fzf/shell/key-bindings.zsh"
-
-# Rebind ctrl+t to ctrl+e as former is occupied by konsole's new tab
-bindkey -r '^T'
-bindkey '^E' fzf-cd-widget
