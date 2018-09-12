b9b787a531df591c8beb18b9c8c1ea195e656481
Igor Urazov <z0rc3r@gmail.com>
2018-09-12 12:15:14 +0300

    Space style change

--- zsh/rc.d/16_x11.zsh
+++ zsh/rc.d/16_x11.zsh
@@ -1,5 +1,5 @@
 # Transfer to root user's Xauth cookies
-if [[ ${EUID} -eq 0 && -n "${SSH_CLIENT}"&& -n "${SUDO_USER}" && -n "${DISPLAY}" ]]; then
+if [[ ${EUID} -eq 0 && -n "${SSH_CLIENT}" && -n "${SUDO_USER}" && -n "${DISPLAY}" ]]; then
     local_display=$(echo ${DISPLAY} | cut -d':' -f 2 | cut -d'.' -f 1)
     cred=$(su - ${SUDO_USER} -c "xauth list" | grep ${local_display})
     echo ${cred} | xargs -n 3 xauth add
