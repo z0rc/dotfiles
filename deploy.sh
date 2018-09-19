2e62eba7f6e33578471b87dfbf9bbefa5584e3eb
Igor Urazov <z0rc3r@gmail.com>
2018-09-19 13:49:12 +0300

    Use --remote option when updating submodules

--- deploy.sh
+++ deploy.sh
@@ -42,7 +42,7 @@ print "  ...done"
 # Make sure submodules are installed
 print "Syncing submodules..."
 git submodule sync > /dev/null
-git submodule update --init --recursive --depth 1 > /dev/null
+git submodule update --init --recursive --remote --no-fetch --depth 1 > /dev/null
 git clean -ffd
 print "  ...done"
 
