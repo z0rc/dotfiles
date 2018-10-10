42ee02668503b3e58f1053308551e9375270dd12
Igor Urazov <z0rc3r@gmail.com>
2018-10-10 16:15:25 +0300

    Switch to bash-style word matching

--- zsh/rc.d/04_autoload.zsh
+++ zsh/rc.d/04_autoload.zsh
@@ -13,3 +13,7 @@ zle -N bracketed-paste bracketed-paste-magic
 autoload -z edit-command-line
 zle -N edit-command-line
 bindkey "^X^E" edit-command-line
+
+# Ctrl+W stops on path delimiters
+autoload -U select-word-style
+select-word-style bash
