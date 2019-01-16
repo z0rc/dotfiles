853ce5c22ebfa048b575c0af36c1d197b8824651
Igor Urazov <z0rc3r@gmail.com>
2019-01-16 16:11:45 +0200

    Temporary disable bracked-paste-magic in zsh
    Let's see how I can work without it.

--- zsh/rc.d/04_autoload.zsh
+++ zsh/rc.d/04_autoload.zsh
@@ -6,8 +6,8 @@ autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
 
 # Bracketed paste magic
-autoload -Uz bracketed-paste-magic
-zle -N bracketed-paste bracketed-paste-magic
+#autoload -Uz bracketed-paste-magic
+#zle -N bracketed-paste bracketed-paste-magic
 
 # Fullscreen command line edit
 autoload -z edit-command-line
--- zsh/rc.d/56_autosuggestions.zsh
+++ zsh/rc.d/56_autosuggestions.zsh
@@ -1,12 +1,9 @@
 # Autosuggestions plugin
 source "${ZDOTDIR}/plugins/autosuggestions/zsh-autosuggestions.zsh"
 # Compatability with bracketed-paste-magic
-function z-asug-d { zle autosuggest-disable; }
-function z-asug-e { zle autosuggest-enable; }
-zstyle :bracketed-paste-magic paste-init z-asug-d
-zstyle :bracketed-paste-magic paste-finish z-asug-e
+#function z-asug-d { zle autosuggest-disable; }
+#function z-asug-e { zle autosuggest-enable; }
+#zstyle :bracketed-paste-magic paste-init z-asug-d
+#zstyle :bracketed-paste-magic paste-finish z-asug-e
 # Enable experimental async autosuggestions
 ZSH_AUTOSUGGEST_USE_ASYNC=1
-# Temporary fix for missing directory suggestions
-# https://github.com/zsh-users/zsh-autosuggestions/issues/379
-#ZSH_AUTOSUGGEST_IGNORE_WIDGETS+=(expand-or-complete fzf-\*)
