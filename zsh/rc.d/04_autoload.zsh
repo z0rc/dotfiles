8c3eae03d843b089be229cad4cb6235d2e217e76
Igor Urazov <z0rc3r@gmail.com>
2019-01-17 18:55:05 +0200

    Cleanup

--- zsh/rc.d/04_autoload.zsh
+++ zsh/rc.d/04_autoload.zsh
@@ -5,10 +5,6 @@ autoload -U colors && colors
 autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
 
-# Bracketed paste magic
-#autoload -Uz bracketed-paste-magic
-#zle -N bracketed-paste bracketed-paste-magic
-
 # Fullscreen command line edit
 autoload -z edit-command-line
 zle -N edit-command-line
--- zsh/rc.d/56_autosuggestions.zsh
+++ zsh/rc.d/56_autosuggestions.zsh
@@ -1,9 +1,4 @@
 # Autosuggestions plugin
 source "${ZDOTDIR}/plugins/autosuggestions/zsh-autosuggestions.zsh"
-# Compatability with bracketed-paste-magic
-#function z-asug-d { zle autosuggest-disable; }
-#function z-asug-e { zle autosuggest-enable; }
-#zstyle :bracketed-paste-magic paste-init z-asug-d
-#zstyle :bracketed-paste-magic paste-finish z-asug-e
 # Enable experimental async autosuggestions
 ZSH_AUTOSUGGEST_USE_ASYNC=1
