5f0c243927ab6af243749906a1c0846c78180904
Igor Urazov <z0rc3r@gmail.com>
2019-04-04 12:04:10 +0300

    Remove zsh-asug start precmd after 2 prompts

--- zsh/rc.d/56_autosuggestions.zsh
+++ zsh/rc.d/56_autosuggestions.zsh
@@ -1,4 +1,19 @@
-# Autosuggestions plugin
-source "${ZDOTDIR}/plugins/autosuggestions/zsh-autosuggestions.zsh"
 # Enable experimental async autosuggestions
 ZSH_AUTOSUGGEST_USE_ASYNC=1
+# Autosuggestions plugin
+source "${ZDOTDIR}/plugins/autosuggestions/zsh-autosuggestions.zsh"
+
+# Remove zsh-asug hook after 2 prompts
+typeset -gi _UNHOOK_ZSH_AUTOSUGGEST_COUNTER=0
+
+function _unhook_autosuggest() {
+  emulate -L zsh
+  if (( ++_UNHOOK_ZSH_AUTOSUGGEST_COUNTER == 2 )); then
+    add-zsh-hook -D precmd _zsh_autosuggest_start
+    add-zsh-hook -D precmd _unhook_autosuggest
+    unset _UNHOOK_ZSH_AUTOSUGGEST_COUNTER
+    unfunction _unhook_autosuggest
+  fi
+}
+
+add-zsh-hook precmd _unhook_autosuggest
