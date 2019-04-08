b3ff18acd783a3712ebcf956bddc24f1136717ac
Igor Urazov <z0rc3r@gmail.com>
2019-04-08 12:25:01 +0300

    Remove tzvt
    It takes to much of vim startup time. Down to 500ms from 800ms on macOS.

--- zsh/rc.d/62_tzvt.zsh
+++
@@ -1,4 +0,0 @@
-source "${ZDOTDIR}/plugins/tmux-zsh-vim-titles/unified-titles.plugin.zsh"
-export tzvt_zsh_update_tmux=true
-export tzvt_tmux_plugin_installed=true
-export tzvt_set_tmux_window_status=true
