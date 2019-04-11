7ed4ffbb67358bbdc35bbc3ae0281a048e4d5783
Igor Urazov <z0rc3r@gmail.com>
2019-04-11 16:11:12 +0300

    Use proper option to stop z-asug rebinds

---
+++ zsh/rc.d/58_autosuggestions.zsh
@@ -0,0 +1,6 @@
+# Enable experimental async autosuggestions
+ZSH_AUTOSUGGEST_USE_ASYNC=1
+# Don't rebind widgets by autosuggestion, it's already sourced pretty late
+ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+# Autosuggestions plugin
+source "${ZDOTDIR}/plugins/autosuggestions/zsh-autosuggestions.zsh"
