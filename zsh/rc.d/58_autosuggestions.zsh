e6bbc415162cc438ce326f4a9cdaf9367aea6319
Igor Urazov <z0rc3r@gmail.com>
2019-04-16 14:02:15 +0300

    Enable experimental completion z-asug

--- zsh/rc.d/58_autosuggestions.zsh
+++ zsh/rc.d/58_autosuggestions.zsh
@@ -2,5 +2,7 @@
 ZSH_AUTOSUGGEST_USE_ASYNC=1
 # Don't rebind widgets by autosuggestion, it's already sourced pretty late
 ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+# Enable experimental completion suggestions, if `history` returns nothing
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 # Autosuggestions plugin
 source "${ZDOTDIR}/plugins/autosuggestions/zsh-autosuggestions.zsh"
