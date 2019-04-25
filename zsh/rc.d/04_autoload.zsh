97c060f9afacbcd834106c3a315078f538d470a0
Igor Urazov <z0rc3r@gmail.com>
2019-04-25 15:19:02 +0300

    Reenable BPM with zsh

--- zsh/rc.d/04_autoload.zsh
+++ zsh/rc.d/04_autoload.zsh
@@ -1,9 +1,9 @@
 # Initialize colors
 autoload -U colors && colors
 
-# URL magic
-autoload -U url-quote-magic
-zle -N self-insert url-quote-magic
+# Bracketed paste magic
+autoload -Uz bracketed-paste-magic
+zle -N bracketed-paste bracketed-paste-magic
 
 # Fullscreen command line edit
 autoload -z edit-command-line
--- zsh/rc.d/58_autosuggestions.zsh
+++ zsh/rc.d/58_autosuggestions.zsh
@@ -4,5 +4,17 @@ ZSH_AUTOSUGGEST_USE_ASYNC=1
 ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 # Enable experimental completion suggestions, if `history` returns nothing
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+
+# Compatability with bracketed paste magic
+pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+}
+pastefinish() {
+  zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(bracketed-paste)
+
 # Autosuggestions plugin
 source "${ZDOTDIR}/plugins/autosuggestions/zsh-autosuggestions.zsh"
