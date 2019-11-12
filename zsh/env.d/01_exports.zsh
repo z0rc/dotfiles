34396b29236fe3c28fcfa97d5da93ab9e2d25e76
Igor Urazov <z0rc3r@gmail.com>
2019-11-12 19:12:40 +0200

    Handle XDG vars more gracefully

--- zsh/env.d/01_exports.zsh
+++ zsh/env.d/01_exports.zsh
@@ -7,9 +7,18 @@ export LESS="--RAW-CONTROL-CHARS --ignore-case --hilite-unread --LONG-PROMPT --w
 export READNULLCMD=${PAGER}
 
 # XDG basedir spec compliance
-export XDG_CACHE_HOME="${HOME}/.cache"
-export XDG_CONFIG_HOME"=${HOME}/.config"
-export XDG_DATA_HOME="${HOME}/.local/share"
+if [[ ! -v XDG_CONFIG_HOME ]]; then
+    export XDG_CONFIG_HOME"=${HOME}/.config"
+fi
+if [[ ! -v XDG_CACHE_HOME ]]; then
+    export XDG_CACHE_HOME="${HOME}/.cache"
+fi
+if [[ ! -v XDG_DATA_HOME ]]; then
+    export XDG_DATA_HOME="${HOME}/.local/share"
+fi
+if [[ ! -v XDG_RUNTIME_DIR ]]; then
+    export XDG_RUNTIME_DIR="${TMPDIR:-/tmp}/runtime-${USER}"
+fi
 
 export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
 export LESSHISTFILE="${XDG_DATA_HOME}/lesshst"
