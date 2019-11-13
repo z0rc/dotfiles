7fc1a013fa922439c3f77b737084919ddefcf1c7
Igor Urazov <z0rc3r@gmail.com>
2019-11-13 13:19:53 +0200

    Refactor rehash precmd function

--- zsh/rc.d/06_rehash.zsh
+++ zsh/rc.d/06_rehash.zsh
@@ -1,9 +1,16 @@
-# Rehash on software upgrade
-autoload -U add-zsh-hook
-TRAPUSR1() { rehash };
+# On USR1 signal do a rehash, to refresh available binaries in $PATH
+TRAPUSR1() { rehash; };
+
+# If previous command was something like package manager, then send USR1 to all zsh processes
 _install_rehash_precmd() {
     if [[ $history[$[HISTCMD-1]] == *(apt|pip|dpkg|yum|rpm|brew|npm|gem|dnf)* ]]; then
-        command ps ux | grep "[z]sh" | awk '{print $2}' | xargs kill -USR1
+        local psux=("${(@f)"$(command ps ux)"}")
+        for zsh_proc in ${(M)psux:#*zsh*~*gitstatusd*}; do
+            kill -USR1 ${${(s: :)zsh_proc}[2]} &>/dev/null &!
+        done
     fi
 }
+
+# Do this check at precmd hook
+autoload -U add-zsh-hook
 add-zsh-hook precmd _install_rehash_precmd
