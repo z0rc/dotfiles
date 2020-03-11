017efbfb1b1b2380bd75b89cdbaa0bf344577717
Igor Urazov <z0rc3r@gmail.com>
2020-03-11 15:32:42 +0200

    Reenable b-p-m and u-q-m again
    Latest workaround for z-asug compat works fine.

--- zsh/rc.d/05_autoload.zsh
+++ zsh/rc.d/05_autoload.zsh
@@ -12,3 +12,24 @@ select-word-style bash
 
 # zrecompile to compile some plugins
 autoload -U zrecompile
+
+# enable url-quote-magic
+autoload -Uz url-quote-magic
+zle -N self-insert url-quote-magic
+
+# enable bracketed paste
+autoload -Uz bracketed-paste-magic
+zle -N bracketed-paste bracketed-paste-magic
+
+# compatability with zsh-asug
+pasteinit() {
+    OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+    zle -N self-insert url-quote-magic
+}
+
+pastefinish() {
+    zle -N self-insert $OLD_SELF_INSERT
+}
+
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
