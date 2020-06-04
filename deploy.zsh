c683b7c928eb02ca688b472d21b4f72c3ddd7ae3
Igor Urazov <z0rc3r@gmail.com>
2020-06-04 11:59:05 +0300

    Add git-fuzzy for trial

--- deploy.zsh
+++ deploy.zsh
@@ -76,6 +76,10 @@ if (( ${+commands[make]} )); then
     print "  ...done"
 fi
 
+print "Installing git-fuzzy..."
+ln -sf "${SCRIPT_DIR}/tools/git-fuzzy/bin/git-fuzzy" "${HOME}/.local/bin/git-fuzzy"
+print "  ...done"
+
 print "Installing fzf..."
 pushd tools/fzf
 if ./install --bin &> /dev/null; then
