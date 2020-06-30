dbae9043a9fb6884b7e150054c50420e23e705fb
Igor Urazov <z0rc3r@gmail.com>
2020-06-30 14:32:52 +0300

    Add proper grc completion
    This should handle aliased commands too.

--- zsh/rc.d/15_grc.zsh
+++ zsh/rc.d/15_grc.zsh
@@ -28,3 +28,26 @@ if (( ${+commands[grc]} )); then
 
     unset grc_commands grc_command
 fi
+
+# custom completion to allow completion for aliased commands
+_grc () {
+    setopt localoptions extended_glob
+    local environ e cmd
+    local -a args
+    local -a _comp_priv_prefix
+    zstyle -a ":completion:${curcontext}:" environ environ
+    for e in "${environ[@]}"; do
+        local -x "${e}"
+    done
+    cmd="${words[1]}"
+    args=(
+        '(-e --stderr)'{-e,--stderr}'[redirect stderr; do not automatically redirect stdout]'
+        '(-s --stdout)'{-s,--stdout}'[redirect stdout; even with -e/--stderr]'
+        '(-c <name>--config=<name>)'{-c+,--config=-}'[use <name> as configuration file for grcat]:file:_files'
+        '--color=-[colo?urize output]:color:(on off auto)'
+        '(-h --help)'{-h,--help}'[display help message and exit]'
+        '--pty[run command in pseudotermnial (experimental)]'
+        '*::arguments:{ _normal }'
+    )
+    _arguments -s -S $args
+}
