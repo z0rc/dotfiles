d2927eeed95c34e0b637c85e4ad1ab9ef1eed767
Igor Urazov <z0rc3r@gmail.com>
2020-08-11 19:48:33 +0300

    Tune abbrs
    Removed global ones, added a bit more for git, enabled highlighting

--- zsh/rc.d/57_syntax_highlight.zsh
+++ zsh/rc.d/57_syntax_highlight.zsh
@@ -1,3 +1,9 @@
 # Highlighting plugin
 source "${ZDOTDIR}/plugins/syntax-highlighting/zsh-syntax-highlighting.zsh"
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+# Highlight known abbrevations
+typeset -A ZSH_HIGHLIGHT_PATTERNS
+for a in $(abbr l); do
+    ZSH_HIGHLIGHT_PATTERNS+=($a 'fg=blue')
+done
+unset a
