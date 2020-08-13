864fe3488894a7638f1d8f3fbefc6a842600fec0
Igor Urazov <z0rc3r@gmail.com>
2020-08-14 00:22:24 +0300

    Don't use subshell to set less colors

--- zsh/rc.d/10_colors.zsh
+++ zsh/rc.d/10_colors.zsh
@@ -1,11 +1,12 @@
 # Color man
-export LESS_TERMCAP_mb=$(printf "\e[1;31m")
-export LESS_TERMCAP_md=$(printf "\e[1;31m")
-export LESS_TERMCAP_me=$(printf "\e[0m")
-export LESS_TERMCAP_se=$(printf "\e[0m")
-export LESS_TERMCAP_so=$(printf "\e[0;37;102m")
-export LESS_TERMCAP_ue=$(printf "\e[0m")
-export LESS_TERMCAP_us=$(printf "\e[4;32m")
+printf -v LESS_TERMCAP_mb "\e[1;31m"
+printf -v LESS_TERMCAP_md "\e[1;31m"
+printf -v LESS_TERMCAP_me "\e[0m"
+printf -v LESS_TERMCAP_se "\e[0m"
+printf -v LESS_TERMCAP_so "\e[0;37;102m"
+printf -v LESS_TERMCAP_ue "\e[0m"
+printf -v LESS_TERMCAP_us "\e[4;32m"
+export LESS_TERMCAP_mb LESS_TERMCAP_md LESS_TERMCAP_me LESS_TERMCAP_se LESS_TERMCAP_so LESS_TERMCAP_ue LESS_TERMCAP_us
 
 # Enable color support of ls
 if (( ${+commands[dircolors]} )); then
