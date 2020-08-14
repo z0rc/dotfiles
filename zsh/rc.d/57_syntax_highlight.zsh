7fabe1c5bc22687e3c035d5c5d09bc50bd01f0a6
Igor Urazov <z0rc3r@gmail.com>
2020-08-14 12:29:50 +0300

    Highlight abbreviations using regexp
    This makes sure they highlighted only when they standalone words, but
    not part of other words.

--- zsh/rc.d/57_syntax_highlight.zsh
+++ zsh/rc.d/57_syntax_highlight.zsh
@@ -1,9 +1,6 @@
 # Highlighting plugin
 source "${ZDOTDIR}/plugins/syntax-highlighting/zsh-syntax-highlighting.zsh"
-ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets regexp cursor)
 # Highlight known abbrevations
-typeset -A ZSH_HIGHLIGHT_PATTERNS
-for a in $(abbr l); do
-    ZSH_HIGHLIGHT_PATTERNS+=($a 'fg=blue')
-done
-unset a
+typeset -A ZSH_HIGHLIGHT_REGEXP
+ZSH_HIGHLIGHT_REGEXP+=('(^| )('${(j:|:)${(k)ABBR_REGULAR_USER_ABBREVIATIONS}}')($| )' 'fg=blue')
