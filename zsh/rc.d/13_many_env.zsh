151f072f6e83ac9e16b4725ab02501e1ecc86ae3
Igor Urazov <z0rc3r@gmail.com>
2021-01-17 13:52:50 +0200

    Replace temporary var unset with anon functions

--- zsh/rc.d/13_many_env.zsh
+++ zsh/rc.d/13_many_env.zsh
@@ -3,14 +3,16 @@ export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 # Lazy init wrapper on first call
-wrappers=(goenv jenv luaenv nodenv phpenv plenv pyenv rbenv)
-for wrapper in "${wrappers[@]}"; do
-    eval "${wrapper} () {
-        unset -f ${wrapper}
-        export ${wrapper:u}_ROOT=\"\${XDG_DATA_HOME}/${wrapper}\"
-        PATH=\"\${DOTFILES}/env-wrappers/${wrapper}/${wrapper}/bin:\${PATH}\"
-        evalcache ${wrapper} init -
-        ${wrapper} \${@}
-    }"
-done
-unset wrappers wrapper
+() {
+    local wrapper
+    local wrappers=(goenv jenv luaenv nodenv phpenv plenv pyenv rbenv)
+    for wrapper in "${wrappers[@]}"; do
+        eval "${wrapper} () {
+            unset -f ${wrapper}
+            export ${wrapper:u}_ROOT=\"\${XDG_DATA_HOME}/${wrapper}\"
+            PATH=\"\${DOTFILES}/env-wrappers/${wrapper}/${wrapper}/bin:\${PATH}\"
+            evalcache ${wrapper} init -
+            ${wrapper} \${@}
+        }"
+    done
+}
--- zsh/rc.d/14_grc.zsh
+++ zsh/rc.d/14_grc.zsh
@@ -1,32 +1,33 @@
 # Alias commands supported by grc
 if (( ${+commands[grc]} )); then
-    grc_commands=(blkid df dig dnf du env free gcc getfacl getsebool
-                  ifconfig ip iptables last lsattr lsblk lsmod lspci
-                  mount mtr netstat nmap ping ps pv semanage ss stat
-                  sysctl systemctl tcpdump traceroute tune2fs ulimit
-                  uptime vmstat wdiff)
+    () {
+        local grc_commands=(blkid df dig dnf du env free gcc getfacl getsebool
+                            ifconfig ip iptables last lsattr lsblk lsmod lspci
+                            mount mtr netstat nmap ping ps pv semanage ss stat
+                            sysctl systemctl tcpdump traceroute tune2fs ulimit
+                            uptime vmstat wdiff)
+        local grc_command
 
-    for grc_command in ${grc_commands[@]}; do
-        if (( ${+commands[$grc_command]} )) && [[ -f "/usr/local/share/grc/conf.${grc_command}" || -f "/usr/share/grc/conf.${grc_command}" ]]; then
-            if (( ${+aliases[$grc_command]} )); then
-                alias ${grc_command}="grc --colour=auto $(whence -f ${grc_command})"
-            else
-                alias ${grc_command}="grc --colour=auto ${grc_command}"
+        for grc_command in ${grc_commands[@]}; do
+            if (( ${+commands[$grc_command]} )) && [[ -f "/usr/local/share/grc/conf.${grc_command}" || -f "/usr/share/grc/conf.${grc_command}" ]]; then
+                if (( ${+aliases[$grc_command]} )); then
+                    alias ${grc_command}="grc --colour=auto $(whence -f ${grc_command})"
+                else
+                    alias ${grc_command}="grc --colour=auto ${grc_command}"
+                fi
             fi
-        fi
-    done
+        done
 
-    for grc_command in w who; do
-        if (( ${+commands[$grc_command]} )); then
-            if (( ${+aliases[$grc_command]} )); then
-                alias ${grc_command}="grc --colour=auto $(whence -f ${grc_command})"
-            else
-                alias ${grc_command}="grc --colour=auto ${grc_command}"
+        for grc_command in w who; do
+            if (( ${+commands[$grc_command]} )); then
+                if (( ${+aliases[$grc_command]} )); then
+                    alias ${grc_command}="grc --colour=auto $(whence -f ${grc_command})"
+                else
+                    alias ${grc_command}="grc --colour=auto ${grc_command}"
+                fi
             fi
-        fi
-    done
-
-    unset grc_commands grc_command
+        done
+    }
 fi
 
 # custom completion to allow completion for aliased commands
