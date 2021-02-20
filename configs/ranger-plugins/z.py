c5d4f1388d1f8623c1868ec2637ab5bbae35f6d5
Igor Urazov <z0rc3r@gmail.com>
2021-02-21 01:49:29 +0200

    Improve ranger's z plugin

--- configs/ranger-plugins/z.py
+++ configs/ranger-plugins/z.py
@@ -4,6 +4,8 @@ from ranger.api.commands import Command
 
 class z(Command):
     def execute(self):
-        directory = subprocess.check_output(["zsh", "-ic", "z -e " + self.arg(1)])
-        directory = directory.decode("utf-8", "ignore").rstrip('\n')
-        self.fm.execute_console("cd " + directory)
+        directory = subprocess.check_output(
+            ["zsh", "-ic", "z -e " + self.arg(1)],
+            text=True
+        ).rstrip("\n")
+        self.fm.cd(directory)
