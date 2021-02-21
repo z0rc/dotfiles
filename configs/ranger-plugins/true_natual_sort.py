ec9d1f5cd337ec6267cb1ffbd26bbf2917d2c074
Igor Urazov <z0rc3r@gmail.com>
2021-02-21 15:05:55 +0200

    sort_unicode was affecting nat sort in ranger

--- configs/ranger-plugins/true_natual_sort.py
+++
@@ -1,5 +0,0 @@
-import re
-from ranger.container.directory import Directory
-
-
-Directory.sort_dict['natural'] = lambda s: [int(t) if t.isdigit() else t.lower() for t in re.split(r'(\d+)', str(s))]
