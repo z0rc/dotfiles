921d86ed19ee3c58a270757233962a3171fbde36
Igor Urazov <z0rc3r@gmail.com>
2021-09-15 17:29:36 +0300

    Test abbr patch for better autosuggestion compat

--- zsh/rc.d/54_abbr.zsh
+++ zsh/rc.d/54_abbr.zsh
@@ -1,2 +1,10 @@
 ABBR_USER_ABBREVIATIONS_FILE="${ZDOTDIR}/plugins/abbreviations-store"
 source "${ZDOTDIR}/plugins/abbr/zsh-abbr.zsh"
+
+# monkey patch abbr for better autosuggestion compatibility
+_abbr_widget_expand_and_space() {
+  emulate -LR zsh
+  _abbr_widget_expand
+  'builtin' 'command' -v _zsh_autosuggest_fetch &>/dev/null && _zsh_autosuggest_fetch
+  zle self-insert
+}
