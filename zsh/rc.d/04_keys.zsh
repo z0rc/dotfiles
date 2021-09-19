e6351807ee366376a402963cdca07835c7e4f87a
Igor Urazov <z0rc3r@gmail.com>
2021-09-19 10:57:42 +0300

    Modernize terminfo zsh stuff

--- zsh/rc.d/04_keys.zsh
+++ zsh/rc.d/04_keys.zsh
@@ -1,19 +1,7 @@
 # Use emacs keybindings even if our EDITOR is set to vi
 bindkey -e
 
-# Make sure that the terminal is in application mode when zle is active, since
-# only then values from $terminfo are valid
 zmodload zsh/terminfo
-if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
-    function zle-line-init() {
-        echoti smkx
-    }
-    function zle-line-finish() {
-        echoti rmkx
-    }
-    zle -N zle-line-init
-    zle -N zle-line-finish
-fi
 
 # Create a zkbd compatible hash
 typeset -A key
@@ -27,20 +15,32 @@ key[Left]=${terminfo[kcub1]}
 key[Right]=${terminfo[kcuf1]}
 key[PageUp]=${terminfo[kpp]}
 key[PageDown]=${terminfo[knp]}
-key[BackSpace]=${terminfo[kbs]}
-# https://invisible-island.net/ncurses/terminfo.src.html#tic-xterm_pcfkeys
+key[Backspace]=${terminfo[kbs]}
+key[ShiftTab]="${terminfo[kcbt]}"
+# man 5 user_caps
 key[CtrlLeft]=${terminfo[kLFT5]}
 key[CtrlRight]=${terminfo[kRIT5]}
 
 # Setup keys accordingly
-[[ -n "${key[Home]}" ]] && bindkey "${key[Home]}" beginning-of-line
-[[ -n "${key[End]}" ]] && bindkey "${key[End]}" end-of-line
-[[ -n "${key[Insert]}" ]] && bindkey "${key[Insert]}" overwrite-mode
-[[ -n "${key[Delete]}" ]] && bindkey "${key[Delete]}" delete-char
-[[ -n "${key[Left]}" ]] && bindkey "${key[Left]}" backward-char
-[[ -n "${key[Right]}" ]] && bindkey "${key[Right]}" forward-char
-[[ -n "${key[PageUp]}" ]] && bindkey "${key[PageUp]}" beginning-of-buffer-or-history
-[[ -n "${key[PageDown]}" ]] && bindkey "${key[PageDown]}" end-of-buffer-or-history
-[[ -n "${key[BackSpace]}" ]] && bindkey "${key[BackSpace]}" backward-delete-char
-[[ -n "${key[CtrlLeft]}" ]] && bindkey "${key[CtrlLeft]}" backward-word
-[[ -n "${key[CtrlRight]}" ]] && bindkey "${key[CtrlRight]}" forward-word
+[[ -n "${key[Home]}"      ]] && bindkey "${key[Home]}"       beginning-of-line
+[[ -n "${key[End]}"       ]] && bindkey "${key[End]}"        end-of-line
+[[ -n "${key[Insert]}"    ]] && bindkey "${key[Insert]}"     overwrite-mode
+[[ -n "${key[Delete]}"    ]] && bindkey "${key[Delete]}"     delete-char
+[[ -n "${key[Left]}"      ]] && bindkey "${key[Left]}"       backward-char
+[[ -n "${key[Right]}"     ]] && bindkey "${key[Right]}"      forward-char
+[[ -n "${key[PageUp]}"    ]] && bindkey "${key[PageUp]}"     beginning-of-buffer-or-history
+[[ -n "${key[PageDown]}"  ]] && bindkey "${key[PageDown]}"   end-of-buffer-or-history
+[[ -n "${key[Backspace]}" ]] && bindkey "${key[Backspace]}"  backward-delete-char
+[[ -n "${key[Shift-Tab]}" ]] && bindkey "${key[Shift-Tab]}"  reverse-menu-complete
+[[ -n "${key[CtrlLeft]}"  ]] && bindkey "${key[CtrlLeft]}"   backward-word
+[[ -n "${key[CtrlRight]}" ]] && bindkey "${key[CtrlRight]}"  forward-word
+
+# Make sure that the terminal is in application mode when zle is active, since
+# only then values from $terminfo are valid
+if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
+    autoload -Uz add-zle-hook-widget
+    function zle_application_mode_start { echoti smkx }
+    function zle_application_mode_stop { echoti rmkx }
+    add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
+    add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
+fi
