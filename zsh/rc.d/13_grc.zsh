91181faac7a27292d302670e413dc20809c738c4
Ihor Urazov <iurazov@healthjoy.com>
2021-11-01 13:24:24 +0200

    Simplify grc configuration
    Don't check on disk configs, use functions instead of aliases.

--- zsh/rc.d/13_grc.zsh
+++ zsh/rc.d/13_grc.zsh
@@ -5,26 +5,14 @@ if (( ${+commands[grc]} )); then
                             ifconfig ip iptables last lsattr lsblk lsmod lspci
                             mount mtr netstat nmap ping ps pv semanage ss stat
                             sysctl systemctl tcpdump traceroute tune2fs ulimit
-                            uptime vmstat wdiff)
+                            uptime vmstat w wdiff who)
         local grc_command
 
         for grc_command in ${grc_commands[@]}; do
-            if (( ${+commands[$grc_command]} )) && [[ -f "/usr/local/share/grc/conf.${grc_command}" || -f "/usr/share/grc/conf.${grc_command}" ]]; then
-                if (( ${+aliases[$grc_command]} )); then
-                    alias ${grc_command}="grc --colour=auto $(whence -f ${grc_command})"
-                else
-                    alias ${grc_command}="grc --colour=auto ${grc_command}"
-                fi
-            fi
-        done
-
-        for grc_command in w who; do
             if (( ${+commands[$grc_command]} )); then
-                if (( ${+aliases[$grc_command]} )); then
-                    alias ${grc_command}="grc --colour=auto $(whence -f ${grc_command})"
-                else
-                    alias ${grc_command}="grc --colour=auto ${grc_command}"
-                fi
+                $grc_command() {
+                    grc --colour=auto ${commands[$0]} "${@}"
+                }
             fi
         done
     }
