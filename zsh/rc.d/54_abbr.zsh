21e93d97f8e05fbb096fe88db766baba67d7872a
Ihor Urazov <iurazov@healthjoy.com>
2022-02-04 17:30:06 +0200

    Enable abbr man page

--- zsh/rc.d/54_abbr.zsh
+++ zsh/rc.d/54_abbr.zsh
@@ -1,5 +1,6 @@
 ABBR_USER_ABBREVIATIONS_FILE="${ZDOTDIR}/plugins/abbreviations-store"
 source "${ZDOTDIR}/plugins/abbr/zsh-abbr.zsh"
+export MANPATH=${ZDOTDIR}/plugins/abbr/man:$MANPATH
 
 # monkey patch abbr for better autosuggestion compatibility
 _abbr_widget_expand_and_space() {
