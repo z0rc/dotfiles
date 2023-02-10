180a2592b585ffd01f91dd2b1ecfd02f674391bf
Ihor <z0rc3r@gmail.com>
2023-02-10 23:22:02 +0200

    Sad cross-platform noises

--- zsh/rc.d/17_gpg_agent.zsh
+++ zsh/rc.d/17_gpg_agent.zsh
@@ -1,4 +1,4 @@
 # remind gpg-agent to update current tty for new shell session
-if (( ${+commands[gpg-connect-agent]} )) && pgrep -q -u "${EUID}" gpg-agent; then
+if (( ${+commands[gpg-connect-agent]} )) && pgrep -u "${EUID}" gpg-agent &>/dev/null; then
     gpg-connect-agent --quiet --no-history updatestartuptty /bye >/dev/null
 fi
