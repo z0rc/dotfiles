98b5f4eab841c877403602ffc31ed42cd5a43ee4
Ihor <z0rc3r@gmail.com>
2023-03-02 22:56:29 +0200

    Fix compatibility between plugins

--- zsh/rc.d/53_autosuggestions.zsh
+++ zsh/rc.d/53_autosuggestions.zsh
@@ -1,17 +1,10 @@
-# Enable experimental async autosuggestions
-ZSH_AUTOSUGGEST_USE_ASYNC=1
 # Don't rebind widgets by autosuggestion, it's already sourced pretty late
 ZSH_AUTOSUGGEST_MANUAL_REBIND=1
 # Enable experimental completion suggestions, if `history` returns nothing
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-# Ignore suggestions for abbreviations
-ZSH_AUTOSUGGEST_HISTORY_IGNORE=${(j:|:)${(Qk)ABBR_REGULAR_USER_ABBREVIATIONS}}
-ZSH_AUTOSUGGEST_COMPLETION_IGNORE=${ZSH_AUTOSUGGEST_HISTORY_IGNORE}
 
 # Autosuggestions plugin
 source "${ZDOTDIR}/plugins/autosuggestions/zsh-autosuggestions.zsh"
 
 # Clear suggestions after paste
-ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(
-    bracketed-paste
-)
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(bracketed-paste)
--- zsh/rc.d/54_abbr.zsh
+++ zsh/rc.d/54_abbr.zsh
@@ -1,3 +1,7 @@
 ABBR_USER_ABBREVIATIONS_FILE="${ZDOTDIR}/plugins/abbreviations-store"
 source "${ZDOTDIR}/plugins/abbr/zsh-abbr.zsh"
 export MANPATH=${ZDOTDIR}/plugins/abbr/man:$MANPATH
+
+# Ignore suggestions for abbreviations
+ZSH_AUTOSUGGEST_HISTORY_IGNORE=${(j:|:)${(Qk)ABBR_REGULAR_USER_ABBREVIATIONS}}
+ZSH_AUTOSUGGEST_COMPLETION_IGNORE=${ZSH_AUTOSUGGEST_HISTORY_IGNORE}
