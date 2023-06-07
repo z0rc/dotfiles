db91ababddf015f702910b59f4a0fe0cd3653b99
Ihor <z0rc3r@gmail.com>
2023-06-07 13:23:05 +0300

    Switch iterm integration to submodule

--- zsh/rc.d/50_iterm.zsh
+++ zsh/rc.d/50_iterm.zsh
@@ -1,4 +1,5 @@
 # iTerm2 integration
 if [[ -v ITERM_PROFILE ]] || [[ -v ITERM_SESSION ]]; then
-    source "${ZDOTDIR}/plugins/iterm2_integration.zsh"
+    source "${ZDOTDIR}/plugins/iterm2-shell-integration/shell_integration/zsh"
+    path=(${ZDOTDIR}/plugins/iterm2-shell-integration/utilities $path)
 fi
