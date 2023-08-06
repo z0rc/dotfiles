95e53812b5e22dcd1025d84589db88dd31a94cbe
Ihor Urazov <z0rc3r@gmail.com>
2023-08-06 14:56:41 +0300

    Unify unsetopt/NO_ style

--- zsh/rc.d/02_setopt.zsh
+++ zsh/rc.d/02_setopt.zsh
@@ -6,13 +6,13 @@ setopt HIST_REDUCE_BLANKS # trim multiple insgnificant blanks in history
 setopt HIST_IGNORE_SPACE # don’t store lines starting with space
 setopt EXTENDED_GLOB # treat special characters as part of patterns
 setopt CORRECT_ALL # try to correct the spelling of all arguments in a line
-setopt NO_FLOW_CONTROL # disable stupid annoying keys
+unsetopt FLOW_CONTROL # disable stupid annoying keys
 setopt MULTIOS # allows multiple input and output redirections
 setopt AUTO_CD # if the command is directory and cannot be executed, perfort cd to this directory
 setopt CLOBBER # allow > redirection to truncate existing files
 setopt BRACE_CCL # allow brace character class list expansion
-setopt NO_BEEP # do not beep on errors
-setopt NO_NOMATCH # try to avoid the 'zsh: no matches found...'
+unsetopt BEEP # do not beep on errors
+unsetopt NOMATCH # try to avoid the 'zsh: no matches found...'
 setopt INTERACTIVE_COMMENTS # allow use of comments in interactive code
 setopt AUTO_PARAM_SLASH # complete folders with / at end
 setopt LIST_TYPES # mark type of completion suggestions
