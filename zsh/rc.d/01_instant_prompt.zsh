1b3c79447dca858573e44cab7131ed90b04e608b
Ihor Urazov <z0rc3r@gmail.com>
2023-10-06 13:19:34 +0300

    Don't clear screen under ssh or sudo

--- zsh/rc.d/01_instant_prompt.zsh
+++ zsh/rc.d/01_instant_prompt.zsh
@@ -1,6 +1,9 @@
 # clear screen, move cursor to the bottom
-autoload -Uz clear-screen-soft-bottom
-clear-screen-soft-bottom
+# but don't do this under ssh or sudo sessions, as prompt already at the bottom
+if ! [[ -v SSH_TTY || -v SUDO_USER ]]; then
+    autoload -Uz clear-screen-soft-bottom
+    clear-screen-soft-bottom
+fi
 
 # powerlevel10k instant promt stanza
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
