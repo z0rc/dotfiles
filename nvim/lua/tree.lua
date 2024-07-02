74090cbf546f8d7893a9d4326e41fb9be153c0d0
Ihor Urazov <z0rc3r@gmail.com>
2024-07-02 19:56:08 +0300

    Add project.nvim

--- nvim/lua/tree.lua
+++ nvim/lua/tree.lua
@@ -2,8 +2,11 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup {
-  git = {
-    timeout = 2000
+  sync_root_with_cwd = true,
+  respect_buf_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_root = true
   },
   tab = {
     sync = {
