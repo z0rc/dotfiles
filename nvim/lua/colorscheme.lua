32eed5fc2589efefd0b7151228d445f0cf60a1a6
Ihor Urazov <z0rc3r@gmail.com>
2024-07-03 13:41:57 +0300

    Fine tune colorscheme and nvim-tree

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,5 +1,12 @@
 require('solarized').setup {
   theme = 'neo',
+  highlights = function(color, helper)
+    return {
+      -- default green color is too contrast for this kind of symbols
+      IblIndent = { fg = helper.darken(color.base01, 30), nocombine = true },
+      IblScope = { fg = color.base01, nocombine = true },
+    }
+  end,
 }
 
 vim.cmd.colorscheme('solarized')
--- nvim/lua/tree.lua
+++ nvim/lua/tree.lua
@@ -47,11 +47,6 @@ require("nvim-tree").setup {
 
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
 local function open_nvim_tree(data)
-  local IGNORED_FT = {
-    "gitcommit",
-    "crontab",
-  }
-
   -- buffer is a real file on the disk
   local real_file = vim.fn.filereadable(data.file) == 1
 
@@ -62,10 +57,20 @@ local function open_nvim_tree(data)
     return
   end
 
+  -- skip files in TMPDIR, kubectl does this when editing resources
+  if string.find(data.file, vim.env.TMPDIR) then
+    return
+  end
+
+  -- skip specific filetypes
+  local IGNORED_FILETYPES = {
+    "gitcommit",
+    "crontab",
+  }
+
   local filetype = vim.bo[data.buf].ft
 
-  -- skip ignored filetypes
-  if vim.tbl_contains(IGNORED_FT, filetype) then
+  if vim.tbl_contains(IGNORED_FILETYPES, filetype) then
     return
   end
 
@@ -76,38 +81,25 @@ end
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
-local function tab_win_closed(winnr)
-  local api = require"nvim-tree.api"
-  local tabnr = vim.api.nvim_win_get_tabpage(winnr)
-  local bufnr = vim.api.nvim_win_get_buf(winnr)
-  local buf_info = vim.fn.getbufinfo(bufnr)[1]
-  local tab_wins = vim.tbl_filter(function(w) return w~=winnr end, vim.api.nvim_tabpage_list_wins(tabnr))
-  local tab_bufs = vim.tbl_map(vim.api.nvim_win_get_buf, tab_wins)
-  if buf_info.name:match(".*NvimTree_%d*$") then            -- close buffer was nvim tree
-    -- Close all nvim tree on :q
-    if not vim.tbl_isempty(tab_bufs) then                      -- and was not the last window (not closed automatically by code below)
-      api.tree.close()
+vim.api.nvim_create_autocmd("QuitPre", {
+  callback = function()
+    local tree_wins = {}
+    local floating_wins = {}
+    local wins = vim.api.nvim_list_wins()
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+      if bufname:match("NvimTree_") ~= nil then
+        table.insert(tree_wins, w)
+      end
+      if vim.api.nvim_win_get_config(w).relative ~= '' then
+        table.insert(floating_wins, w)
+      end
     end
-  else                                                      -- else closed buffer was normal buffer
-    if #tab_bufs == 1 then                                    -- if there is only 1 buffer left in the tab
-      local last_buf_info = vim.fn.getbufinfo(tab_bufs[1])[1]
-      if last_buf_info.name:match(".*NvimTree_%d*$") then       -- and that buffer is nvim tree
-        vim.schedule(function ()
-          if #vim.api.nvim_list_wins() == 1 then                -- if its the last buffer in vim
-            vim.cmd "quit"                                        -- then close all of vim
-          else                                                  -- else there are more tabs open
-            vim.api.nvim_win_close(tab_wins[1], true)             -- then close only the tab
-          end
-        end)
+    if 1 == #wins - #floating_wins - #tree_wins then
+      -- Should quit, so we close all invalid windows.
+      for _, w in ipairs(tree_wins) do
+        vim.api.nvim_win_close(w, true)
       end
     end
   end
-end
-
-vim.api.nvim_create_autocmd("WinClosed", {
-  callback = function ()
-    local winnr = tonumber(vim.fn.expand("<amatch>"))
-    vim.schedule_wrap(tab_win_closed(winnr))
-  end,
-  nested = true
 })
