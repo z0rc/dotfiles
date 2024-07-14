cc242ae459033111f6188d0fcd87a0de28faa5b1
Ihor Urazov <z0rc3r@gmail.com>
2024-07-15 01:11:58 +0300

    Initial which-key setup

---
+++ nvim/lua/whichkey.lua
@@ -0,0 +1,21 @@
+local wk = require('which-key')
+
+wk.setup({
+  icons = {
+    rules = false,
+    keys = {
+      Up = "↑",
+      Down = "↓",
+      Left = "←",
+      Right = "→",
+      C = "Ctrl",
+      M = "Alt",
+      S = "Shift",
+      CR = "↵",
+      Esc = "Esc",
+      BS = "⌫",
+      Space = "Space",
+      Tab = "⇥",
+    },
+  },
+})
