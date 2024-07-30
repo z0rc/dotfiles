69b6b77dbc2fb7dc897310852e27e28206762577
Ihor Urazov <z0rc3r@gmail.com>
2024-07-30 15:05:54 +0300

    Disable netrw earlier

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -26,4 +26,7 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.lsp.set_log_level(vim.log.levels.OFF)
--- nvim/lua/tree.lua
+++ nvim/lua/tree.lua
@@ -1,6 +1,3 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 require('nvim-tree').setup {
   sync_root_with_cwd = true,
   respect_buf_cwd = true,
