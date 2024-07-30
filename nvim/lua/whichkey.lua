32e1805c3573905042f199eda4240db4e4946f65
Ihor Urazov <z0rc3r@gmail.com>
2024-07-30 15:22:27 +0300

    Drop more nerd-font bullshit

--- nvim/lua/whichkey.lua
+++ nvim/lua/whichkey.lua
@@ -3,6 +3,7 @@ local wk = require('which-key')
 wk.setup({
   icons = {
     rules = false,
+    mappings = false,
     keys = {
       Up = '↑',
       Down = '↓',
@@ -16,6 +17,18 @@ wk.setup({
       BS = '⌫',
       Space = 'Space',
       Tab = '⇥',
+      F1 = 'F1',
+      F2 = 'F2',
+      F3 = 'F3',
+      F4 = 'F4',
+      F5 = 'F5',
+      F6 = 'F6',
+      F7 = 'F7',
+      F8 = 'F8',
+      F9 = 'F9',
+      F10 = 'F10',
+      F11 = 'F11',
+      F12 = 'F12',
     },
   },
 })
