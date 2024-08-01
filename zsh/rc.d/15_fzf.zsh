02b3703db6967609751eb9913cf7826c73032d2a
Ihor Urazov <z0rc3r@gmail.com>
2024-08-01 12:27:20 +0300

    Fixup fzf stuff

--- zsh/rc.d/15_fzf.zsh
+++ zsh/rc.d/15_fzf.zsh
@@ -1,9 +1,9 @@
 export FZF_DEFAULT_OPTS="--ansi"
 # Try to use fd or ag, if available as default fzf command
-if (( ${+commands[fd1]} )); then
+if (( ${+commands[fd]} )); then
     export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git --color=always'
     export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
-elif (( ${+commands[rg1]} )); then
+elif (( ${+commands[rg]} )); then
     export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --color=always --glob "!.git/*"'
     export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
 elif (( ${+commands[ag]} )); then
