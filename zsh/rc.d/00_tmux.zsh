3a0626c9d010928ff5e027ff1fe76c07822aada9
Ihor Urazov <z0rc3r@gmail.com>
2024-10-08 19:31:20 +0300

    Skip tmux autostart on root

--- zsh/rc.d/00_tmux.zsh
+++ zsh/rc.d/00_tmux.zsh
@@ -1,5 +1,5 @@
-# Start tmux, if it's first terminal tab, skip this on remote sessions
+# Start tmux, if it's first terminal tab, skip this on remote sessions and root/sudo
 # Handoff to tmux early, as rest of the rc config isn't needed for this
-if (( ${+commands[tmux]} )) && [[ ! -v TMUX ]] && [[ ! -v SSH_TTY ]] && ! tmux list-sessions &>/dev/null; then
+if (( ${+commands[tmux]} )) && [[ ! -v TMUX && ! -v SSH_TTY && ${EUID} != 0 ]] && ! tmux list-sessions &>/dev/null; then
     exec tmux new-session
 fi
