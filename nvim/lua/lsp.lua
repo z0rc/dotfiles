08e9176f8549c14f84c2176ea071bbc46fe37000
Ihor Urazov <z0rc3r@gmail.com>
2024-11-14 00:21:32 +0200

    Disable terraformls syntax highlight
    It's garbage compared to treesitter

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -103,5 +103,13 @@ require('mason-lspconfig').setup {
         },
       })
     end,
+    terraformls = function()
+      require('lspconfig').terraformls.setup {
+        -- disable this lsp server syntax highlighting, it's garbage compared to what treesitter provides
+        on_init = function(client)
+          client.server_capabilities.semanticTokensProvider = nil
+        end,
+      }
+    end,
   }
 }
