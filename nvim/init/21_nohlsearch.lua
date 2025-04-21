a6ee98a44659496412cc2491354c44f34156fd73
Ihor Urazov <z0rc3r@gmail.com>
2025-04-21 16:47:06 +0300

    Replace vim-cool with nvim custom logic

---
+++ nvim/init/21_nohlsearch.lua
@@ -0,0 +1,10 @@
+-- Disable highlighting of search results when moving the cursor
+-- https://www.reddit.com/r/neovim/comments/1ct2w2h/comment/l4bgvn1/
+vim.api.nvim_create_autocmd('CursorMoved', {
+  group = vim.api.nvim_create_augroup('auto-hlsearch', { clear = true }),
+  callback = function()
+    if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+      vim.schedule(function() vim.cmd.nohlsearch() end)
+    end
+  end
+})
