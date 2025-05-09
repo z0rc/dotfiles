6467beea6eae1f860511dcff0c9b9731173dec29
Ihor Urazov <z0rc3r@gmail.com>
2025-05-09 11:57:38 +0300

    Fix quotes style

--- nvim/init/18_lsp.lua
+++ nvim/init/18_lsp.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable-next-line: missing-fields
 require('lazydev').setup {
   library = {
-    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
   },
 }
 
