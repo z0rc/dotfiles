2429aae71796c1e9537cf75aa55c02911c84d860
Ihor Urazov <z0rc3r@gmail.com>
2025-07-12 22:50:03 +0300

    Style fixes

--- nvim/init/11_keymaps.lua
+++ nvim/init/11_keymaps.lua
@@ -32,17 +32,17 @@ end, { remap = true, expr = true, desc = 'Duplicate and comment lines' })
 local gitsigns = require('gitsigns')
 vim.keymap.set('n', ']c', function()
   if vim.wo.diff then
-    vim.cmd.normal { ']c', bang = true }
+    vim.cmd.normal({ ']c', bang = true })
   else
-    gitsigns.nav_hunk 'next'
+    gitsigns.nav_hunk('next')
   end
 end, { desc = 'Jump to next git [c]hange' })
 
 vim.keymap.set('n', '[c', function()
   if vim.wo.diff then
-    vim.cmd.normal { '[c', bang = true }
+    vim.cmd.normal({ '[c', bang = true })
   else
-    gitsigns.nav_hunk 'prev'
+    gitsigns.nav_hunk('prev')
   end
 end, { desc = 'Jump to previous git [c]hange' })
 
