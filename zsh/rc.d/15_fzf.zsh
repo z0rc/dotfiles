6afa4ef2fc527260a9f213eaa5ee9eb57e2c0a4b
Ihor Urazov <z0rc3r@gmail.com>
2025-10-01 17:56:17 +0300

    Refresh fzf stuff, cleanup fpath functions

--- zsh/rc.d/15_fzf.zsh
+++ zsh/rc.d/15_fzf.zsh
@@ -1,14 +1,2 @@
-export FZF_DEFAULT_OPTS="--ansi"
-# Try to use fd or rg or ag, if available as default fzf command
-if (( ${+commands[fd]} )); then
-    export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git --color=always'
-    export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
-elif (( ${+commands[rg]} )); then
-    export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --color=always --glob "!.git/*"'
-    export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
-elif (( ${+commands[ag]} )); then
-    export FZF_DEFAULT_COMMAND='ag --ignore .git --color -g ""'
-    export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
-fi
-
-source "${DOTFILES}/tools/fzf/shell/key-bindings.zsh"
+# Some sane defaults for fzf
+export FZF_DEFAULT_OPTS="--ansi --height=50% --tmux=bottom,50%,border-native"
