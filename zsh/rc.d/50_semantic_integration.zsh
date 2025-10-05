4a8dfcd11ff4835e94fc5109b2e6ab903710325e
Ihor Urazov <z0rc3r@gmail.com>
2025-10-05 15:43:06 +0300

    Enable semantic integration for konsole

---
+++ zsh/rc.d/50_semantic_integration.zsh
@@ -0,0 +1,23 @@
+# iTerm2 integration
+if [[ -v ITERM_PROFILE || -v ITERM_SESSION ]]; then
+    export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=1
+    source $ZDOTDIR/plugins/iterm2-shell-integration/shell_integration/zsh
+    path=($ZDOTDIR/plugins/iterm2-shell-integration/utilities $path)
+fi
+
+# Konsole integration
+# Based on https://www.reddit.com/r/kde/comments/zf1ehj/psa_konsole_2208_now_supports_semantic_shell/ and P10K native support
+if [[ -v KONSOLE_VERSION ]]; then
+    export POWERLEVEL9K_TERM_SHELL_INTEGRATION=true
+
+    _konsole_precmd() {
+        print -n "\e]133;L\a\e]133;D;$?\a"
+    }
+
+    _konsole_preexec() {
+        print -n "\e]133;C\a"
+    }
+
+    add-zsh-hook precmd _konsole_precmd
+    add-zsh-hook preexec _konsole_preexec
+fi
