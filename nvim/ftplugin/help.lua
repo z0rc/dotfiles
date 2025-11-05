3d9cba929c9c5044e4166fa8f43d903eff4ccb28
Ihor Urazov <z0rc3r@gmail.com>
2025-11-05 12:26:53 +0200

    Better help view experience

--- nvim/ftplugin/help.lua
+++ nvim/ftplugin/help.lua
@@ -1,1 +1,7 @@
+vim.bo.bufhidden = 'unload'
+vim.wo.sidescrolloff = 0
+
+vim.cmd.wincmd('L')
+vim.cmd('vertical resize 79')
+
 vim.treesitter.start()
--- nvim/init/12_tree.lua
+++ nvim/init/12_tree.lua
@@ -1,5 +1,4 @@
 require('nvim-tree').setup({
-  sync_root_with_cwd = true,
   respect_buf_cwd = true,
   update_focused_file = {
     enable = true,
