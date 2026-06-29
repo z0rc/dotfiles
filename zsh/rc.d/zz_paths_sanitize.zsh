9c1c07c2d510a4b789de5581c38090bb891b048a
Ihor Urazov <z0rc3r@gmail.com>
2026-06-29 17:28:36 +0300

    Sanitize MANPATH even more

--- zsh/rc.d/zz_paths_sanitize.zsh
+++ zsh/rc.d/zz_paths_sanitize.zsh
@@ -1,2 +1,11 @@
 # Force path arrays to have unique values only
 typeset -U path cdpath fpath manpath
+
+# Reformat MANPATH into expected format, so it ends with colon
+# Ghostty breaks it with own env var append logic
+local mpath="" manpath_old=($manpath)
+MANPATH=""
+for mpath in $manpath_old; do
+    printf -v MANPATH "%s%s:" $MANPATH $mpath
+done
+unset mpath manpath_old
