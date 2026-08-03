58a82c0d5d9804655df947682a0b3025839f5aad
Ihor Urazov <z0rc3r@gmail.com>
2026-08-03 16:04:59 +0300

    Refresh gpg tty for ssh too

--- zsh/rc.d/18_gpg_agent.zsh
+++ zsh/rc.d/18_gpg_agent.zsh
@@ -1,7 +1,7 @@
-# Remind gpg-agent to update current tty before running git
+# Remind gpg-agent to update current tty before running git or ssh
 if (( ${+commands[gpg-connect-agent]} )) && pgrep -u $EUID gpg-agent &>/dev/null; then
     function _preexec_gpg-agent-update-tty {
-        if [[ $1 == git* ]]; then
+        if [[ $1 == (git|ssh)* ]]; then
             gpg-connect-agent --quiet --no-autostart updatestartuptty /bye &>/dev/null
         fi
     }
