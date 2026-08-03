a54dabed2d76a7248cf45c987b25f026b2c4d197
Ihor Urazov <z0rc3r@gmail.com>
2026-08-03 15:55:05 +0300

    Rework CSIu bindkeys

--- zsh/rc.d/05_keys.zsh
+++ zsh/rc.d/05_keys.zsh
@@ -41,40 +41,74 @@ key[CtrlRight]=${terminfo[kRIT5]}
 unset key
 
 # Also bind some 'CSI u' keys, https://www.leonerd.org.uk/hacks/fixterms/
+#
+#   cursor keys    CSI <letter>      modified:  CSI 1 ; <modifier> <letter>
+#   special keys   CSI <number> ~    modified:  CSI <number> ; <modifier> ~
+#   any other key                    modified:  CSI <codepoint> ; <modifier> u
+#
+# where <modifier> is 1 + Shift(1) + Alt(2) + Ctrl(4), so Alt is 3 and Ctrl is 5
 typeset -A csi
-csi[base]="\e["
-csi[really-special-prefix]=${csi[base]}"1;"
-csi[special-suffix]="~"
-csi[modifier-Ctrl]="5"
-csi[special-Insert]="2"
-csi[special-Delete]="3"
-csi[special-PageUp]="5"
-csi[special-PageDown]="6"
-csi[special-Home]="7"
-csi[special-End]="8"
-csi[exception-ShiftTab]="Z"
-csi[really-special-Up]="A"
-csi[really-special-Down]="B"
-csi[really-special-Right]="C"
-csi[really-special-Left]="D"
-csi[really-special-End]="F"
-csi[really-special-Home]="H"
 
-bindkey ${csi[base]}${csi[really-special-Home]}                                         beginning-of-line
-bindkey ${csi[base]}${csi[really-special-End]}                                          end-of-line
-bindkey ${csi[base]}${csi[special-Home]}${csi[special-suffix]}                          beginning-of-line
-bindkey ${csi[base]}${csi[special-End]}${csi[special-suffix]}                           end-of-line
-bindkey ${csi[base]}${csi[special-Insert]}${csi[special-suffix]}                        overwrite-mode
-bindkey ${csi[base]}${csi[special-Delete]}${csi[special-suffix]}                        delete-char
-bindkey ${csi[base]}${csi[special-Left]}${csi[special-suffix]}                          backward-char
-bindkey ${csi[base]}${csi[special-Right]}${csi[special-suffix]}                         forward-char
-bindkey ${csi[base]}${csi[special-Up]}${csi[special-suffix]}                            up-line-or-beginning-search
-bindkey ${csi[base]}${csi[special-Down]}${csi[special-suffix]}                          down-line-or-beginning-search
-bindkey ${csi[base]}${csi[special-PageUp]}${csi[special-suffix]}                        beginning-of-buffer-or-history
-bindkey ${csi[base]}${csi[special-PageDown]}${csi[special-suffix]}                      end-of-buffer-or-history
-bindkey ${csi[base]}${csi[exception-ShiftTab]}                                          reverse-menu-complete
-bindkey ${csi[really-special-prefix]}${csi[modifier-Ctrl]}${csi[really-special-Left]}   backward-word
-bindkey ${csi[really-special-prefix]}${csi[modifier-Ctrl]}${csi[really-special-Right]}  forward-word
+# Cursor keys 'CSI <letter>'
+csi[Up]="\e[A"
+csi[Down]="\e[B"
+csi[Right]="\e[C"
+csi[Left]="\e[D"
+csi[End]="\e[F"
+csi[Home]="\e[H"
+# Cursor keys with Ctrl 'CSI 1 ; 5 <letter>'
+csi[Ctrl-Up]="\e[1;5A"
+csi[Ctrl-Down]="\e[1;5B"
+csi[Ctrl-Right]="\e[1;5C"
+csi[Ctrl-Left]="\e[1;5D"
+csi[Ctrl-End]="\e[1;5F"
+csi[Ctrl-Home]="\e[1;5H"
+# Cursor keys with Alt 'CSI 1 ; 3 <letter>'
+csi[Alt-Right]="\e[1;3C"
+csi[Alt-Left]="\e[1;3D"
+# Special keys 'CSI <number> ~'
+csi[Insert]="\e[2~"
+csi[Delete]="\e[3~"
+csi[PageUp]="\e[5~"
+csi[PageDown]="\e[6~"
+csi[Home-tilde]="\e[7~"
+csi[End-tilde]="\e[8~"
+# Special keys with Ctrl 'CSI <number> ; 5 ~'
+csi[Ctrl-Delete]="\e[3;5~"
+csi[Ctrl-Home-tilde]="\e[7;5~"
+csi[Ctrl-End-tilde]="\e[8;5~"
+# Encoded keys 'CSI <codepoint> ; <modifier> u'
+csi[Ctrl-Backspace]="\e[127;5u"
+csi[Alt-Backspace]="\e[127;3u"
+# Spec exception
+csi[Shift-Tab]="\e[Z"
+
+bindkey ${csi[Up]}               up-line-or-beginning-search
+bindkey ${csi[Down]}             down-line-or-beginning-search
+bindkey ${csi[Right]}            forward-char
+bindkey ${csi[Left]}             backward-char
+bindkey ${csi[End]}              end-of-line
+bindkey ${csi[Home]}             beginning-of-line
+bindkey ${csi[Ctrl-Up]}          up-line-or-beginning-search
+bindkey ${csi[Ctrl-Down]}        down-line-or-beginning-search
+bindkey ${csi[Ctrl-Right]}       forward-word
+bindkey ${csi[Ctrl-Left]}        backward-word
+bindkey ${csi[Ctrl-End]}         end-of-buffer-or-history
+bindkey ${csi[Ctrl-Home]}        beginning-of-buffer-or-history
+bindkey ${csi[Alt-Right]}        forward-word
+bindkey ${csi[Alt-Left]}         backward-word
+bindkey ${csi[Insert]}           overwrite-mode
+bindkey ${csi[Delete]}           delete-char
+bindkey ${csi[PageUp]}           beginning-of-buffer-or-history
+bindkey ${csi[PageDown]}         end-of-buffer-or-history
+bindkey ${csi[Home-tilde]}       beginning-of-line
+bindkey ${csi[End-tilde]}        end-of-line
+bindkey ${csi[Ctrl-Delete]}      kill-word
+bindkey ${csi[Ctrl-Home-tilde]}  beginning-of-buffer-or-history
+bindkey ${csi[Ctrl-End-tilde]}   end-of-buffer-or-history
+bindkey ${csi[Ctrl-Backspace]}   backward-kill-word
+bindkey ${csi[Alt-Backspace]}    backward-kill-word
+bindkey ${csi[Shift-Tab]}        reverse-menu-complete
 unset csi
 
 # Make dot key autoexpand "..." to "../.." and so on
