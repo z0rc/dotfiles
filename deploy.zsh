ccacc4a84b6e4c6ab76c8d9acec54e397c9ae2c9
Ihor Urazov <z0rc3r@gmail.com>
2026-08-17 18:51:39 +0300

    tig doesn't use cache dir

--- deploy.zsh
+++ deploy.zsh
@@ -21,7 +21,7 @@ XDG_STATE_HOME=$HOME/.local/state
 # Create required directories
 print "Creating required directory tree..."
 zf_mkdir -p $XDG_CONFIG_HOME/{ghostty,git/local,htop,ranger,gem,tig,gnupg,nvim/{plugin,after},yazi}
-zf_mkdir -p $XDG_CACHE_HOME/{vim/{backup,swap,undo},zsh,tig}
+zf_mkdir -p $XDG_CACHE_HOME/{vim/{backup,swap,undo},zsh}
 zf_mkdir -p $XDG_DATA_HOME/{{goenv,jenv,luaenv,nodenv,phpenv,plenv,pyenv,rbenv}/plugins,zsh,man/man1,vim/spell,nvim/site/pack/plugins}
 zf_mkdir -p $XDG_STATE_HOME
 zf_mkdir -p $HOME/.local/{bin,etc}
