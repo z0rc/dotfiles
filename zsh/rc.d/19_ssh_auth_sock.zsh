c619c51f75e447861c90fedbc29f1cc4fc51ea63
Ihor Urazov <z0rc3r@gmail.com>
2026-08-18 00:52:50 +0300

    Refactor ssh_auth_sock symlink logic

--- zsh/rc.d/19_ssh_auth_sock.zsh
+++ zsh/rc.d/19_ssh_auth_sock.zsh
@@ -1,15 +1,17 @@
 # Maintain SSH_AUTH_SOCK to link at well-known places
-if [[ EUID -ne 0 && -d $HOME/.ssh ]]; then
-    if [[ -S $GNUPGHOME/S.gpg-agent.ssh ]]; then
-        zf_ln -sfn $GNUPGHOME/S.gpg-agent.ssh $HOME/.ssh/ssh_auth_sock
-    elif [[ -S /run/user/$EUID/gnupg/S.gpg-agent.ssh ]]; then
-        zf_ln -sfn /run/user/$EUID/gnupg/S.gpg-agent.ssh $HOME/.ssh/ssh_auth_sock
-    elif [[ -S /run/user/$EUID/gnupg/*/S.gpg-agent.ssh(#qN) ]]; then
-        zf_ln -sfn /run/user/$EUID/gnupg/*/S.gpg-agent.ssh $HOME/.ssh/ssh_auth_sock
-    elif [[ -S $XDG_RUNTIME_DIR/ssh-agent.socket ]]; then
-        zf_ln -sfn $XDG_RUNTIME_DIR/ssh-agent.socket $HOME/.ssh/ssh_auth_sock
+if (( EUID != 0 )) && [[ -d $HOME/.ssh ]]; then
+    # `-=` matches sockets following symlinks, `om` prefers the newest
+    local -a ssh_socks=(
+        $GNUPGHOME/S.gpg-agent.ssh(#qN-=)
+        /run/user/$EUID/gnupg/S.gpg-agent.ssh(#qN-=)
+        /run/user/$EUID/gnupg/*/S.gpg-agent.ssh(#qN-=om)
+        $XDG_RUNTIME_DIR/ssh-agent.socket(#qN-=)
+    )
+    if (( $#ssh_socks )); then
+        zf_ln -sfn $ssh_socks[1] $HOME/.ssh/ssh_auth_sock
     elif [[ -S $SSH_AUTH_SOCK && ! -h $SSH_AUTH_SOCK && $SSH_AUTH_SOCK != $HOME/.ssh/ssh_auth_sock ]]; then
         zf_ln -sfn $SSH_AUTH_SOCK $HOME/.ssh/ssh_auth_sock
     fi
     export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
+    unset ssh_socks
 fi
