aac5aac71c709300004aabb5515b54f66532abd0
Ihor Urazov <z0rc3r@gmail.com>
2026-08-22 15:55:27 +0300

    Replace execs with socket checks

--- zsh/rc.d/18_gpg_agent.zsh
+++ zsh/rc.d/18_gpg_agent.zsh
@@ -1,5 +1,5 @@
 # Remind gpg-agent to update current tty before running git or ssh
-if (( ${+commands[gpg-connect-agent]} )) && pgrep -u $EUID gpg-agent &>/dev/null; then
+if (( ${+commands[gpg-connect-agent]} )) && [[ -S $GNUPGHOME/S.gpg-agent ]]; then
     function _preexec_gpg-agent-update-tty {
         if [[ $1 == (git|ssh)* ]]; then
             gpg-connect-agent --quiet --no-autostart updatestartuptty /bye &>/dev/null
