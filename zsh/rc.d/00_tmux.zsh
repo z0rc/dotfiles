95beb164280e0998ad8b5ffe34e59d0dac9d1d50
Ihor Urazov <z0rc3r@gmail.com>
2026-08-24 13:49:09 +0300

    Make tmux socket existence check more robust

--- zsh/rc.d/00_tmux.zsh
+++ zsh/rc.d/00_tmux.zsh
@@ -1,5 +1,12 @@
 # Start tmux, if it's first terminal tab, skip this on remote sessions and root/sudo
 # Handoff to tmux early, as rest of the rc config isn't needed for this
-if (( ${+commands[tmux]} )) && [[ ! -v TMUX && ! -v SSH_TTY ]] && (( EUID != 0 )) && [[ ! -S ${TMUX_TMPDIR:-/tmp}/tmux-$UID/default ]]; then
-    exec tmux new-session
+if (( ${+commands[tmux]} )) && [[ ! -v TMUX && ! -v SSH_TTY ]] && (( EUID != 0 )); then
+    zmodload -F zsh/net/socket b:zsocket 2>/dev/null
+    if ! zsocket ${TMUX_TMPDIR:-/tmp}/tmux-$UID/default 2>/dev/null; then
+        exec tmux new-session
+    else
+        # close file descriptor, so it doesn't leak
+        exec {REPLY}>&-
+        unset REPLY
+    fi
 fi
