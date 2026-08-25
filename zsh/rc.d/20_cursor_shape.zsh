4eb2801c530458281645f5367568c478455b2604
Ihor Urazov <z0rc3r@gmail.com>
2026-08-25 15:35:15 +0300

    Move cursor shape hooks to zle

--- zsh/rc.d/20_cursor_shape.zsh
+++ zsh/rc.d/20_cursor_shape.zsh
@@ -2,7 +2,12 @@
 # https://invisible-island.net/ncurses/terminfo.ti.html#toc-_X_T_E_R_M__Features
 # Ss - set cursor shape, usually 6 as argument means I-beam
 # Se - reset cursor shape, which is usually block
+#
+# TODO: drop all of this once zsh ships the native zle_cursorform array
+# (post-5.9), check for it with (( ${+parameters[zle_cursorform]} ))
 if (( ${+terminfo[Ss]} && ${+terminfo[Se]} )); then
+    autoload -Uz add-zle-hook-widget
+
     _zsh_cursor_shape_reset() {
         echoti Se
     }
@@ -11,6 +16,7 @@ if (( ${+terminfo[Ss]} && ${+terminfo[Se]} )); then
         echoti Ss 6
     }
 
-    add-zsh-hook preexec _zsh_cursor_shape_reset
-    add-zsh-hook precmd _zsh_cursor_shape_ibeam
+    add-zle-hook-widget -Uz zle-line-init   _zsh_cursor_shape_ibeam
+    add-zle-hook-widget -Uz zle-line-finish _zsh_cursor_shape_reset
+    add-zsh-hook zshexit _zsh_cursor_shape_reset
 fi
