2cb183c351cca253dfd0af654590f6b6b45431cc
Ihor Urazov <z0rc3r@gmail.com>
2026-08-30 23:06:55 +0300

    Cache resolved autopair widgets

--- zsh/rc.d/25_autopair.zsh
+++ zsh/rc.d/25_autopair.zsh
@@ -1,2 +1,17 @@
 # Autopairs plugin
+
+# If cache is newer that 20 hours, then use it
+if [[ -n $XDG_CACHE_HOME/zsh/autopair-widgets.zsh(#qN.mh-20) ]]; then
+    source $XDG_CACHE_HOME/zsh/autopair-widgets.zsh 
+fi
+
 source $ZDOTDIR/plugins/autopair/autopair.zsh
+
+# If cache is older that 20 hours, then update it
+{
+    if [[ -n $XDG_CACHE_HOME/zsh/autopair-widgets.zsh(#qN.mh+20) ]]; then
+        print -r -- "AUTOPAIR_SPC_WIDGET=$AUTOPAIR_SPC_WIDGET
+AUTOPAIR_BKSPC_WIDGET=$AUTOPAIR_BKSPC_WIDGET
+AUTOPAIR_DELWORD_WIDGET=$AUTOPAIR_DELWORD_WIDGET" > $XDG_CACHE_HOME/zsh/autopair-widgets.zsh
+    fi
+} &!
