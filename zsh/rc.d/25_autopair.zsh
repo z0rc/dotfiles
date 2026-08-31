32b3c3307f608cbf779782189c1fb3a81dc53e25
Ihor Urazov <z0rc3r@gmail.com>
2026-08-31 13:59:48 +0300

    Fix autopairs caching

--- zsh/rc.d/25_autopair.zsh
+++ zsh/rc.d/25_autopair.zsh
@@ -1,17 +1,15 @@
 # Autopairs plugin
 
-# If cache is newer that 20 hours, then use it
-if [[ -n $XDG_CACHE_HOME/zsh/autopair-widgets.zsh(#qN.mh-20) ]]; then
-    source $XDG_CACHE_HOME/zsh/autopair-widgets.zsh 
+# Use cache of resolved widgets
+local _autopair_cache=$XDG_CACHE_HOME/zsh/autopair-widgets.zsh
+if [[ -n $_autopair_cache(#qN.mh-20) ]]; then
+    source $_autopair_cache
 fi
 
 source $ZDOTDIR/plugins/autopair/autopair.zsh
 
-# If cache is older that 20 hours, then update it
-{
-    if [[ -n $XDG_CACHE_HOME/zsh/autopair-widgets.zsh(#qN.mh+20) ]]; then
-        print -r -- "AUTOPAIR_SPC_WIDGET=$AUTOPAIR_SPC_WIDGET
-AUTOPAIR_BKSPC_WIDGET=$AUTOPAIR_BKSPC_WIDGET
-AUTOPAIR_DELWORD_WIDGET=$AUTOPAIR_DELWORD_WIDGET" > $XDG_CACHE_HOME/zsh/autopair-widgets.zsh
-    fi
-} &!
+# Cache missing or older than 20 hours, refresh it
+if [[ -z $_autopair_cache(#qN.mh-20) ]]; then
+    typeset -p AUTOPAIR_SPC_WIDGET AUTOPAIR_BKSPC_WIDGET AUTOPAIR_DELWORD_WIDGET > $_autopair_cache
+fi
+unset _autopair_cache
